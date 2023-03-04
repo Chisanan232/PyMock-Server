8146be3ce820a7ee33df3c64ee11c2c9e4ddab59
“Chisanan232” <chi10211201@cycu.org.tw>
2023-03-04 23:48:14 +0800

    [Refactor] (test) Refactor to extend the common class and decorate with it.

--- test/integration_test/_utils/reader.py
+++ test/integration_test/_utils/reader.py
@@ -1,8 +1,8 @@
-import os
-from abc import ABCMeta, abstractmethod
+from abc import ABC
 
 import pytest
-from yaml import dump
+
+from .._spec import TestSpecWithFileOpt
 
 try:
     from yaml import CDumper as Dumper
@@ -15,45 +15,9 @@ from pymock_api.model.api_config import APIConfig
 from ..._values import _Test_Config_Value
 
 
-class ReaderTestSpec(metaclass=ABCMeta):
-    @property
-    @abstractmethod
-    def file_path(self) -> str:
-        pass
-
-    def _write_test_file(self) -> None:
-        with open(self.file_path, "a+", encoding="utf-8") as file_stream:
-            output = dump(_Test_Config_Value, Dumper=Dumper)
-            file_stream.writelines(output)
-
-    def _delete_file(self) -> None:
-        if self._exist_file():
-            os.remove(self.file_path)
-        exist_file = self._exist_file()
-        assert exist_file is False, ""
-
-    def _exist_file(self) -> bool:
-        return os.path.exists(self.file_path)
-
+class ReaderTestSpec(TestSpecWithFileOpt, ABC):
     @staticmethod
     def _test_with_file(function):
-        def _(self):
-            # Ensure that it doesn't have file
-            self._delete_file()
-            # Create the target file before run test
-            self._write_test_file()
-
-            try:
-                # Run the test item
-                function(self)
-            finally:
-                # Delete file finally
-                self._delete_file()
-
-        return _
-
-    @staticmethod
-    def _test_with_file_with_reader(function):
         def _(self, reader):
             # Ensure that it doesn't have file
             self._delete_file()
@@ -79,7 +43,7 @@ class TestYAMLReader(ReaderTestSpec):
     def file_path(self) -> str:
         return "./test.yaml"
 
-    @ReaderTestSpec._test_with_file_with_reader
+    @ReaderTestSpec._test_with_file
     def test_open_and_read(self, reader: YAMLReader):
         # Run target function
         reading_data = reader.read(config=self.file_path)
@@ -95,7 +59,7 @@ class TestYAMLReader(ReaderTestSpec):
         # Verify result
         assert isinstance(deserialized_data, APIConfig) and len(deserialized_data) != 0, ""
 
-    @ReaderTestSpec._test_with_file_with_reader
+    @ReaderTestSpec._test_with_file
     def test_load(self, reader: YAMLReader):
         # Run target function
         loaded_data = reader.load(config=self.file_path)
