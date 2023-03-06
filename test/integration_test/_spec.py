71efd1906426b7e024e18d698c92851f0857f43b
“Chisanan232” <chi10211201@cycu.org.tw>
2023-03-06 16:11:57 +0800

    [New Feature] (test) 2 changes:
    1. New common function for writing data to file.
    
    2. New Argument of common function 'path'.

--- test/integration_test/_spec.py
+++ test/integration_test/_spec.py
@@ -1,5 +1,7 @@
+import json
 import os
 from abc import ABCMeta, abstractmethod
+from typing import Optional, Union
 
 from yaml import CDumper as Dumper
 from yaml import dump
@@ -18,14 +20,23 @@ class SpecWithFileOpt(metaclass=ABCMeta):
             output = dump(_Test_Config_Value, Dumper=Dumper)
             file_stream.writelines(output)
 
-    def _delete_file(self) -> None:
-        if self._exist_file():
-            os.remove(self.file_path)
-        exist_file = self._exist_file()
-        assert exist_file is False, ""
+    def _write_json_file(self, path: str, content: Union[str, dict]) -> None:
+        with open(path, "a+", encoding="utf-8") as file_stream:
+            output = json.dumps(content)
+            file_stream.writelines(output)
 
-    def _exist_file(self) -> bool:
-        return os.path.exists(self.file_path)
+    def _delete_file(self, path: Optional[str] = None) -> None:
+        if not path:
+            path = self.file_path
+        if self._exist_file(path):
+            os.remove(path)
+        exist_file = self._exist_file(path)
+        assert exist_file is False, "File should already be removed."
+
+    def _exist_file(self, path: Optional[str] = None) -> bool:
+        if not path:
+            path = self.file_path
+        return os.path.exists(path)
 
     @staticmethod
     def _test_with_file(function):
