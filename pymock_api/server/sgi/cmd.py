9fb665464364bbfe105d0d47500fe6dbcaeba45b
“Chisanan232” <chi10211201@cycu.org.tw>
2023-03-12 21:57:36 +0800

    [New Feature] (code) Add new template method 'generate' for command line object.

--- pymock_api/server/sgi/cmd.py
+++ pymock_api/server/sgi/cmd.py
@@ -1,11 +1,32 @@
 from abc import ABCMeta, abstractmethod
 from typing import Generic
 
+from ._model import Command, CommandOptions, ParserArguments
 from .cmdoption import Base_Command_Option_Type, WSGICmdOption
 
 
 class BaseSGICmd(metaclass=ABCMeta):
     """*Base class of SGI*"""
+
+    def generate(self, parser_args: ParserArguments) -> Command:
+        """Generate an object about command line for running finally.
+
+        Args:
+            parser_args (ParserArguments): The data object which has been parsed by arguments of current running command
+                line.
+
+        Returns:
+            An **Command** type object.
+
+        """
+        return Command(
+            entry_point=self.entry_point,
+            options=CommandOptions(
+                bind=self.options.bind(address=parser_args.bind),
+                workers=self.options.workers(w=parser_args.workers),
+                log_level=self.options.log_level(level=parser_args.log_level),
+            ),
+        )
 
     @property
     @abstractmethod
