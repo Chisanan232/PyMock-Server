57aa5fe57cfdf42ecab7118395953a0119d0caf5
“Chisanan232” <chi10211201@cycu.org.tw>
2023-03-12 22:03:18 +0800

    [New Feature] (code) Add imports and function.

--- pymock_api/server/sgi/__init__.py
+++ pymock_api/server/sgi/__init__.py
@@ -2,3 +2,22 @@
 
 The factory to generate SGI application instance to run Python web framework.
 """
+
+from argparse import Namespace
+
+from ._model import Command, CommandOptions, Deserialize, ParserArguments
+from .cmd import WSGICmd
+from .cmdoption import WSGICmdOption
+
+
+def deserialize_parser_args(args: Namespace) -> ParserArguments:
+    """Deserialize the object *argparse.Namespace* to *ParserArguments*.
+
+    Args:
+        args (Namespace): The arguments which be parsed from current command line.
+
+    Returns:
+        A *ParserArguments* type object.
+
+    """
+    return Deserialize.parser_arguments(args)
