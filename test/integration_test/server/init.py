a5e11c34870c9e3678d896599e6ca5825976c6c7
“Chisanan232” <chi10211201@cycu.org.tw>
2023-03-16 22:36:26 +0800

    Remove tests of unused object and modify data type.

--- test/integration_test/server/init.py
+++ test/integration_test/server/init.py
@@ -1,8 +1,7 @@
 import re
-import sys
 from abc import abstractmethod
-from typing import Type, Union
-from unittest.mock import Mock
+from typing import Type
+from unittest.mock import Mock, patch
 
 import flask
 import pytest
@@ -11,14 +10,13 @@ import pymock_api.server as mock_server
 
 from .._spec import SpecWithFileOpt
 
-SETUP_APP_TYPE = Type[mock_server.SetupApp]
 LOAD_APP_TYPE = Type[mock_server.load_app]
 
 
 class ServerInitTestSpec(SpecWithFileOpt):
     @pytest.fixture(scope="function")
     @abstractmethod
-    def initial_app(self) -> Union[SETUP_APP_TYPE, LOAD_APP_TYPE]:
+    def initial_app(self) -> LOAD_APP_TYPE:
         pass
 
     @property
@@ -43,44 +41,20 @@ class ServerInitTestSpec(SpecWithFileOpt):
         return _
 
 
-class TestSetupApp(ServerInitTestSpec):
-    @pytest.fixture(scope="function")
-    def initial_app(self) -> SETUP_APP_TYPE:
-        return mock_server.SetupApp
-
-    @ServerInitTestSpec._test_with_file
-    def test_by_flask(self, initial_app: SETUP_APP_TYPE):
-        initial_app.by_flask()
-        assert isinstance(mock_server.flask_app, flask.Flask)
-
-    @pytest.mark.skip(reason="Not finish this test because not sure how to mock 'import'.")
-    @SpecWithFileOpt._test_with_file
-    def test_by_flask_when_cannot_import(self):
-        assert "flask" in sys.modules
-        mock_import = Mock(side_effect=ImportError("Import error named 'flask'"))
-        sys.modules["flask"] = mock_import
-        import pymock_api.server as sut_server
-
-        with pytest.raises(RuntimeError) as exc_info:
-            sut_server.SetupApp.by_flask()
-        mock_import.assert_called_once()
-        assert re.search(r"Cannot load.[0,256]cannot import 'flask'.[0,256]", str(exc_info.value))
-
-
 class TestLoadApp(ServerInitTestSpec):
     @pytest.fixture(scope="function")
     def initial_app(self) -> LOAD_APP_TYPE:
         return mock_server.load_app
 
     @ServerInitTestSpec._test_with_file
-    def test_with_flask(self, initial_app: LOAD_APP_TYPE):
-        initial_app.with_flask()
+    def test_by_flask(self, initial_app: LOAD_APP_TYPE):
+        initial_app.by_flask()
         assert isinstance(mock_server.flask_app, flask.Flask)
 
-    @pytest.mark.skip(reason="Not finish this test because not sure how to mock 'import'.")
-    @ServerInitTestSpec._test_with_file
-    def test_with_flask_when_cannot_import(self, initial_app: LOAD_APP_TYPE):
-        sys.modules.pop("flask")
+    @SpecWithFileOpt._test_with_file
+    @patch.object(mock_server.FlaskServer, "setup", side_effect=RuntimeError("Import error for PyTest"))
+    def test_by_flask_when_cannot_import(self, mock_flask_server: Mock):
         with pytest.raises(RuntimeError) as exc_info:
-            initial_app.with_flask()
-        assert re.search(r"Cannot load.[0,256]cannot import.[0,256]", str(exc_info.value))
+            mock_server.load_app.by_flask()
+        mock_flask_server.assert_called_once()
+        assert re.search(r"Import error for PyTest", str(exc_info.value))
--- test/unit_test/server/init.py
+++ test/unit_test/server/init.py
@@ -1,4 +1,3 @@
-import sys
 from typing import Type
 from unittest.mock import Mock, patch
 
@@ -12,10 +11,10 @@ mock_flask_server = Mock(mock_server.FlaskServer())
 mock_server_obj = Mock(mock_server.MockHTTPServer)
 
 
-class TestSetupApp:
+class TestLoadApp:
     @pytest.fixture(scope="function")
-    def setup_app(self) -> Type[mock_server.SetupApp]:
-        return mock_server.SetupApp
+    def load_app(self) -> Type[mock_server.load_app]:
+        return mock_server.load_app
 
     @patch("pymock_api.server.MockHTTPServer", return_value=mock_server_obj)
     @patch("pymock_api.server.FlaskServer", return_value=mock_flask_server)
@@ -25,9 +24,9 @@ class TestSetupApp:
         mock_get_os_env: Mock,
         mock_flask_server_obj: Mock,
         mock_http_server: Mock,
-        setup_app: Type[mock_server.SetupApp],
+        load_app: Type[mock_server.load_app],
     ):
-        setup_app.by_flask()
+        load_app.by_flask()
         mock_get_os_env.assert_called_once_with("MockAPI_Config", "api.yaml")
         mock_flask_server_obj.assert_called_once()
         mock_http_server.assert_called_once_with(
@@ -35,71 +34,22 @@ class TestSetupApp:
         )
 
     @patch("os.environ.get", return_value=_Test_Config)
-    def test_inner_get_config_path(self, mock_get_os_env: Mock, setup_app: Type[mock_server.SetupApp]):
-        path = setup_app._get_config_path()
+    def test_inner_get_config_path(self, mock_get_os_env: Mock, load_app: Type[mock_server.load_app]):
+        path = load_app._get_config_path()
         mock_get_os_env.assert_called_once_with("MockAPI_Config", "api.yaml")
         assert path == _Test_Config
 
     @patch("pymock_api.server.MockHTTPServer", return_value=mock_server_obj)
-    def test_initial_mock_server(self, mock_http_server: Mock, setup_app: Type[mock_server.SetupApp]):
-        server = setup_app._initial_mock_server(config_path=_Test_Config, app_server=mock_flask_server)
+    def test_initial_mock_server(self, mock_http_server: Mock, load_app: Type[mock_server.load_app]):
+        server = load_app._initial_mock_server(config_path=_Test_Config, app_server=mock_flask_server)
         mock_http_server.assert_called_once_with(
             config_path=_Test_Config, app_server=mock_flask_server, auto_setup=True
         )
         assert server == mock_server_obj
 
 
-class TestLoadApp:
-    @pytest.fixture(scope="function")
-    def load_app(self) -> Type[mock_server.load_app]:
-        return mock_server.load_app
-
-    @patch.object(mock_server.SetupApp, "by_flask")
-    def test_with_flask(self, mock_setup_by_flask: Mock, load_app: Type[mock_server.load_app]):
-        mock_import_flask = Mock()
-        sys.modules["flask"] = mock_import_flask
-
-        load_app.with_flask()
-
-        # mock_import_flask.assert_called_once()
-        mock_setup_by_flask.assert_called_once()
-
-    def test_inner_load_app_by_importing(self, load_app: Type[mock_server.load_app]):
-        mock_import_function = Mock()
-        mock_success_function = Mock()
-        mock_fail_function = Mock()
-
-        load_app._load_app_by_importing(
-            import_callback=mock_import_function,
-            import_success_callback=mock_success_function,
-            import_err_callback=mock_fail_function,
-        )
-
-        mock_import_function.assert_called_once()
-        mock_success_function.assert_called_once()
-        mock_fail_function.assert_not_called()
-
-    def test_bad_inner_load_app_by_importing(self, load_app: Type[mock_server.load_app]):
-        test_exc = ImportError("This is PyTest exception.")
-
-        def _import() -> None:
-            raise test_exc
-
-        def _handle_success() -> None:
-            pass
-
-        def _handle_error(e: Exception) -> None:
-            raise e
-
-        with pytest.raises(ImportError) as exc_info:
-            load_app._load_app_by_importing(
-                import_callback=_import, import_success_callback=_handle_success, import_err_callback=_handle_error
-            )
-        assert str(exc_info.value) == str(test_exc)
-
-
 class TestCreateAppFunctions:
-    @patch.object(mock_server.load_app, "with_flask")
+    @patch.object(mock_server.load_app, "by_flask")
     def test_create_flask_app(self, mock_load_app: Mock):
         mock_server.create_flask_app()
         mock_load_app.assert_called_once()
