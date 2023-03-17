f8cfc01aa7b3158beaca3d410d0d8105cf48fa78
“Chisanan232” <chi10211201@cycu.org.tw>
2023-03-17 20:18:50 +0800

    Remove unused abstract class.

--- test/integration_test/server/init.py
+++ test/integration_test/server/init.py
@@ -1,5 +1,4 @@
 import re
-from abc import abstractmethod
 from typing import Type
 from unittest.mock import Mock, patch
 
@@ -13,21 +12,14 @@ from .._spec import run_test
 LOAD_APP_TYPE = Type[mock_server.load_app]
 
 
-class ServerInitTestSpec:
+class TestLoadApp:
     @pytest.fixture(scope="function")
-    @abstractmethod
-    def initial_app(self) -> LOAD_APP_TYPE:
-        pass
-
-
-class TestLoadApp(ServerInitTestSpec):
-    @pytest.fixture(scope="function")
-    def initial_app(self) -> LOAD_APP_TYPE:
+    def load_app(self) -> LOAD_APP_TYPE:
         return mock_server.load_app
 
     @run_test.with_file
-    def test_by_flask(self, initial_app: LOAD_APP_TYPE):
-        initial_app.by_flask()
+    def test_by_flask(self, load_app: LOAD_APP_TYPE):
+        load_app.by_flask()
         assert isinstance(mock_server.flask_app, flask.Flask)
 
     @run_test.with_file
