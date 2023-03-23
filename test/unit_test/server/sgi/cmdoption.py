76b0c0b351487c4733582a1df8bc9e20f2c4cb28
“Chisanan232” <chi10211201@cycu.org.tw>
2023-03-23 21:17:10 +0800

    Extract an base test class which has multiple template tests and open multiple functions about expected value for overriding if it needs.

--- test/unit_test/server/sgi/cmdoption.py
+++ test/unit_test/server/sgi/cmdoption.py
@@ -1,22 +1,34 @@
+from abc import ABCMeta, abstractmethod
 from typing import List, Tuple
 
 import pytest
 
-from pymock_api.server.sgi.cmdoption import ASGICmdOption, WSGICmdOption
+from pymock_api.server.sgi.cmdoption import (
+    ASGICmdOption,
+    BaseCommandOption,
+    WSGICmdOption,
+)
 
 from ...._values import _Bind_Host_And_Port, _Log_Level, _Workers_Amount
 
 
-class TestWSGICmdOption:
+class BaseCommandOptionTest(metaclass=ABCMeta):
     @pytest.fixture(scope="function")
-    def cmd_option(self) -> WSGICmdOption:
-        return WSGICmdOption()
+    @abstractmethod
+    def cmd_option(self) -> BaseCommandOption:
+        pass
 
-    def test_help(self, cmd_option: WSGICmdOption):
-        assert cmd_option.help() == "--help"
+    def test_help(self, cmd_option: BaseCommandOption):
+        assert cmd_option.help() == self._expected_help_option()
 
-    def test_version(self, cmd_option: WSGICmdOption):
-        assert cmd_option.version() == "--version"
+    def _expected_help_option(self) -> str:
+        return "--help"
+
+    def test_version(self, cmd_option: BaseCommandOption):
+        assert cmd_option.version() == self._expected_version_option()
+
+    def _expected_version_option(self) -> str:
+        return "--version"
 
     @pytest.mark.parametrize(
         ("address", "args", "expect"),
@@ -25,74 +37,15 @@ class TestWSGICmdOption:
             (_Bind_Host_And_Port.value.split(":"), ("host", "port"), _Bind_Host_And_Port.value),
         ],
     )
-    def test_bind(self, cmd_option: WSGICmdOption, address: List[str], args: Tuple[str], expect: str):
+    def test_bind(self, cmd_option: BaseCommandOption, address: List[str], args: Tuple[str], expect: str):
         bind_args = {}
         for arg, ad in zip(args, address):
             bind_args[arg] = ad
 
-        assert cmd_option.bind(**bind_args) == f"--bind {expect}"
+        assert cmd_option.bind(**bind_args) == self._expected_bind_option(expect)
 
-    @pytest.mark.parametrize(
-        ("address", "args", "exc_type", "err_msg"),
-        [
-            (
-                [None, "test"],
-                ("host", "port"),
-                ValueError,
-                "There are 2 ways to pass arguments: using *address* or using *host* and *port*.",
-            ),
-            (
-                ["test", None],
-                ("host", "port"),
-                ValueError,
-                "There are 2 ways to pass arguments: using *address* or using *host* and *port*.",
-            ),
-        ],
-    )
-    def test_bind_with_invalid_args(
-        self, cmd_option: WSGICmdOption, address: List[str], args: Tuple[str], exc_type: Exception, err_msg: str
-    ):
-        bind_args = {}
-        for arg, ad in zip(args, address):
-            bind_args[arg] = ad
-        with pytest.raises(exc_type) as exc_info:
-            cmd_option.bind(**bind_args)
-
-        assert str(exc_info.value) == err_msg
-
-    def test_workers(self, cmd_option: WSGICmdOption):
-        workers_amt = _Workers_Amount.value
-        assert cmd_option.workers(w=workers_amt) == f"--workers {workers_amt}"
-
-    def test_log_level(self, cmd_option: WSGICmdOption):
-        log_level = _Log_Level.value
-        assert cmd_option.log_level(level=log_level) == f"--log-level {log_level}"
-
-
-class TestASGICmdOption:
-    @pytest.fixture(scope="function")
-    def cmd_option(self) -> ASGICmdOption:
-        return ASGICmdOption()
-
-    def test_help(self, cmd_option: ASGICmdOption):
-        assert cmd_option.help() == "--help"
-
-    def test_version(self, cmd_option: ASGICmdOption):
-        assert cmd_option.version() == "--version"
-
-    @pytest.mark.parametrize(
-        ("address", "args", "expect"),
-        [
-            ([_Bind_Host_And_Port.value], ("address",), _Bind_Host_And_Port.value.split(":")),
-            (_Bind_Host_And_Port.value.split(":"), ("host", "port"), _Bind_Host_And_Port.value.split(":")),
-        ],
-    )
-    def test_bind(self, cmd_option: ASGICmdOption, address: List[str], args: Tuple[str], expect: List[str]):
-        bind_args = {}
-        for arg, ad in zip(args, address):
-            bind_args[arg] = ad
-
-        assert cmd_option.bind(**bind_args) == f"--host {expect[0]} --port {expect[1]}"
+    def _expected_bind_option(self, expect: str) -> str:
+        return f"--bind {expect}"
 
     @pytest.mark.parametrize(
         ("address", "args", "exc_type", "err_msg"),
@@ -118,7 +71,7 @@ class TestASGICmdOption:
         ],
     )
     def test_bind_with_invalid_args(
-        self, cmd_option: ASGICmdOption, address: List[str], args: Tuple[str], exc_type: Exception, err_msg: str
+        self, cmd_option: BaseCommandOption, address: List[str], args: Tuple[str], exc_type: Exception, err_msg: str
     ):
         bind_args = {}
         for arg, ad in zip(args, address):
@@ -128,10 +81,32 @@ class TestASGICmdOption:
 
         assert str(exc_info.value) == err_msg
 
-    def test_workers(self, cmd_option: ASGICmdOption):
+    def test_workers(self, cmd_option: BaseCommandOption):
         workers_amt = _Workers_Amount.value
-        assert cmd_option.workers(w=workers_amt) == f"--workers {workers_amt}"
+        assert cmd_option.workers(w=workers_amt) == self._expected_workers_option(workers_amt)
 
-    def test_log_level(self, cmd_option: ASGICmdOption):
+    def _expected_workers_option(self, workers_amt: int) -> str:
+        return f"--workers {workers_amt}"
+
+    def test_log_level(self, cmd_option: BaseCommandOption):
         log_level = _Log_Level.value
-        assert cmd_option.log_level(level=log_level) == f"--log-level {log_level}"
+        assert cmd_option.log_level(level=log_level) == self._expected_log_level_option(log_level)
+
+    def _expected_log_level_option(self, log_level: str) -> str:
+        return f"--log-level {log_level}"
+
+
+class TestWSGICmdOption(BaseCommandOptionTest):
+    @pytest.fixture(scope="function")
+    def cmd_option(self) -> WSGICmdOption:
+        return WSGICmdOption()
+
+
+class TestASGICmdOption(BaseCommandOptionTest):
+    @pytest.fixture(scope="function")
+    def cmd_option(self) -> ASGICmdOption:
+        return ASGICmdOption()
+
+    def _expected_bind_option(self, expect: str) -> str:
+        expect_info = expect.split(":")
+        return f"--host {expect_info[0]} --port {expect_info[1]}"
