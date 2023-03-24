b0b8466f8eda3f9cff32dc5b079f0cb79092c82d
“Chisanan232” <chi10211201@cycu.org.tw>
2023-03-24 10:28:26 +0800

    Add unit test for new importing.

--- test/unit_test/_utils/importing.py
+++ test/unit_test/_utils/importing.py
@@ -2,6 +2,7 @@ import re
 from typing import Type
 from unittest.mock import Mock, patch
 
+import fastapi
 import flask
 import pytest
 
@@ -17,34 +18,47 @@ class TestImportWebLib:
     def import_web_lib(self) -> Type[import_web_lib]:
         return import_web_lib
 
-    def test_import_web_lib(self, import_web_lib: Type[import_web_lib]):
+    def test_import_web_lib_flask(self, import_web_lib: Type[import_web_lib]):
         assert import_web_lib.flask() == flask
 
-
-@patch.object(import_web_lib, "flask")
-def test_ensure_importing(mock_import_lib: Mock):
-    fake_function_with_decorator = ensure_importing(import_web_lib.flask)(fake_function)
-    assert fake_function_with_decorator() == "Fake function for PyTest"
-    mock_import_lib.assert_called_once()
+    def test_import_web_lib_fastapi(self, import_web_lib: Type[import_web_lib]):
+        assert import_web_lib.fastapi() == fastapi
 
 
-@patch.object(import_web_lib, "flask", side_effect=ImportError("No module named 'flask'"))
-def test_ensure_importing_fail(mock_import_lib: Mock):
-    with pytest.raises(RuntimeError) as exc_info:
-        fake_function_with_decorator = ensure_importing(import_web_lib.flask)(fake_function)
-        fake_function_with_decorator()
-    mock_import_lib.assert_called_once()
-    assert re.search(r"Cannot load.{0,256}cannot import.{0,256}", str(exc_info.value))
+@pytest.mark.parametrize("web_lib", ["flask", "fastapi"])
+def test_ensure_importing(web_lib: str):
+    with patch.object(import_web_lib, web_lib) as mock_import_lib:
+        import_web_lib_func = getattr(import_web_lib, web_lib)
+        fake_function_with_decorator = ensure_importing(import_web_lib_func)(fake_function)
+        assert fake_function_with_decorator() == "Fake function for PyTest"
+        mock_import_lib.assert_called_once()
 
 
-@patch.object(import_web_lib, "flask", side_effect=ImportError("No module named 'flask'"))
-def test_ensure_importing_fail_with_error_handle(mock_import_lib: Mock):
+@pytest.mark.parametrize("web_lib", ["flask", "fastapi"])
+def test_ensure_importing_fail(web_lib: str):
+    with patch.object(
+        import_web_lib, web_lib, side_effect=ImportError(f"No module named '{web_lib}'")
+    ) as mock_import_lib:
+        import_web_lib_func = getattr(import_web_lib, web_lib)
+        with pytest.raises(RuntimeError) as exc_info:
+            fake_function_with_decorator = ensure_importing(import_web_lib_func)(fake_function)
+            fake_function_with_decorator()
+        mock_import_lib.assert_called_once()
+        assert re.search(r"Cannot load.{0,256}cannot import.{0,256}", str(exc_info.value))
+
+
+@pytest.mark.parametrize("web_lib", ["flask", "fastapi"])
+def test_ensure_importing_fail_with_error_handle(web_lib: str):
     mock_error_handle = Mock()
-    with pytest.raises(RuntimeError) as exc_info:
-        fake_function_with_decorator = ensure_importing(import_web_lib.flask, import_err_callback=mock_error_handle)(
-            fake_function
-        )
-        fake_function_with_decorator()
-    mock_import_lib.assert_called_once()
-    mock_error_handle.assert_called_once()
-    assert re.search(r"Cannot load.{0,256}cannot import.{0,256}", str(exc_info.value))
+    with patch.object(
+        import_web_lib, web_lib, side_effect=ImportError(f"No module named '{web_lib}'")
+    ) as mock_import_lib:
+        import_web_lib_func = getattr(import_web_lib, web_lib)
+        with pytest.raises(RuntimeError) as exc_info:
+            fake_function_with_decorator = ensure_importing(import_web_lib_func, import_err_callback=mock_error_handle)(
+                fake_function
+            )
+            fake_function_with_decorator()
+        mock_import_lib.assert_called_once()
+        mock_error_handle.assert_called_once()
+        assert re.search(r"Cannot load.{0,256}cannot import.{0,256}", str(exc_info.value))
