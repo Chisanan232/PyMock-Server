b76747094645e01ad4a08ac5037b54fc959c49c8
“Chisanan232” <chi10211201@cycu.org.tw>
2023-03-26 11:50:30 +0800

    Refactor the implementation about generating Python code.

--- pymock_api/server/application.py
+++ pymock_api/server/application.py
@@ -6,11 +6,11 @@ This module provides which library of Python web framework you could use to set 
 import json
 import os
 from abc import ABCMeta, abstractmethod
-from typing import Any, List, Union
+from typing import Any, List, Optional, Union
 
 from .._utils.importing import import_web_lib
 from ..exceptions import FileFormatNotSupport
-from ..model.api_config import MockAPIs
+from ..model.api_config import MockAPI, MockAPIs
 
 
 class BaseAppServer(metaclass=ABCMeta):
@@ -36,8 +36,22 @@ class BaseAppServer(metaclass=ABCMeta):
         """
         pass
 
-    @abstractmethod
     def create_api(self, mocked_apis: MockAPIs) -> None:
+        for api_name, api_config in mocked_apis.apis.items():
+            annotate_function_pycode = self._annotate_function(api_name, api_config)
+            add_api_pycode = self._add_api(api_name, api_config, base_url=mocked_apis.base.url)
+            # pylint: disable=exec-used
+            exec(annotate_function_pycode)
+            # pylint: disable=exec-used
+            exec(add_api_pycode)
+
+    def _annotate_function(self, api_name: str, api_config: MockAPI) -> str:
+        return f"""def {api_name}() -> Union[str, dict]:
+            return _HTTPResponse.generate(data='{api_config.http.response.value}')
+        """
+
+    @abstractmethod
+    def _add_api(self, api_name: str, api_config: MockAPI, base_url: Optional[str] = None) -> str:
         pass
 
 
@@ -49,20 +63,17 @@ class FlaskServer(BaseAppServer):
         app: "flask.Flask" = flask_pkg.Flask(__name__)
         return app
 
-    def create_api(self, mocked_apis: MockAPIs) -> None:
-        for api_name, api_config in mocked_apis.apis.items():
-            # pylint: disable=exec-used
-            exec(
-                f"""def {api_name}() -> Union[str, dict]:
-                return _HTTPResponse.generate(data='{api_config.http.response.value}')
+    def _add_api(self, api_name: str, api_config: MockAPI, base_url: Optional[str] = None) -> str:
+        if base_url:
+            return f"""self.web_application.route(
+                "{base_url}{api_config.url}", methods=["{api_config.http.request.method}"]
+                )({api_name})
             """
-            )
-            # pylint: disable=exec-used
-            exec(
-                f"""self.web_application.route(
-                    "{mocked_apis.base.url}{api_config.url}", methods=["{api_config.http.request.method}"]
-                )({api_name}) """
-            )
+        else:
+            return f"""self.web_application.route(
+                "{api_config.url}", methods=["{api_config.http.request.method}"]
+                )({api_name})
+            """
 
 
 class FastAPIServer(BaseAppServer):
@@ -73,20 +84,17 @@ class FastAPIServer(BaseAppServer):
         app: "fastapi.FastAPI" = fastapi_pkg.FastAPI()
         return app
 
-    def create_api(self, mocked_apis: MockAPIs) -> None:
-        for api_name, api_config in mocked_apis.apis.items():
-            # pylint: disable=exec-used
-            exec(
-                f"""def {api_name}() -> Union[str, dict]:
-                return _HTTPResponse.generate(data='{api_config.http.response.value}')
+    def _add_api(self, api_name: str, api_config: MockAPI, base_url: Optional[str] = None) -> str:
+        if base_url:
+            return f"""self.web_application.api_route(
+                path="{base_url}{api_config.url}", methods=["{api_config.http.request.method}"]
+                )({api_name})
             """
-            )
-            # pylint: disable=exec-used
-            exec(
-                f"""self.web_application.api_route(
-                    path="{mocked_apis.base.url}{api_config.url}", methods=["{api_config.http.request.method}"]
-                )({api_name}) """
-            )
+        else:
+            return f"""self.web_application.api_route(
+                path="{api_config.url}", methods=["{api_config.http.request.method}"]
+                )({api_name})
+            """
 
 
 class _HTTPResponse:
