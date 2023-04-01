4458bc3f056538c3e645a08c9cd128fc435799ef
“Chisanan232” <chi10211201@cycu.org.tw>
2023-04-01 13:01:52 +0800

    Comment out deprecated code.

--- pymock_api/_utils/__init__.py
+++ pymock_api/_utils/__init__.py
@@ -1,12 +1,12 @@
-"""*Sub-package for utility functions*"""
-
-from ..model import APIConfig
-from .converter import Convert
-from .importing import ensure_importing, import_web_lib
-from .reader import YAMLReader
-
-
-def load_config(config_path: str) -> APIConfig:
-    yreader = YAMLReader()
-    config = yreader.load(config=config_path)
-    return config
+# """*Sub-package for utility functions*"""
+#
+# from ..model import APIConfig
+# from .converter import Convert
+# from .importing import ensure_importing, import_web_lib
+# from .reader import YAMLReader
+#
+#
+# def load_config(config_path: str) -> APIConfig:
+#     yreader = YAMLReader()
+#     config = yreader.load(config=config_path)
+#     return config
