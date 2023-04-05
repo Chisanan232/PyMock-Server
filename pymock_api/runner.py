44b7de66f9ac3c4d5d56e122964e304e377bc846
“Chisanan232” <chi10211201@cycu.org.tw>
2023-04-05 13:25:30 +0800

    Refactor to use module 'cmd_ps' to implement running logic.

--- pymock_api/runner.py
+++ pymock_api/runner.py
@@ -7,6 +7,7 @@ from typing import List, Optional, Union
 
 try:
     import pymock_api.cmd
+    import pymock_api.cmd_ps
     from pymock_api._utils.file_opt import YAML
     from pymock_api.model import (
         ParserArguments,
@@ -21,6 +22,7 @@ except (ImportError, ModuleNotFoundError):
     path = str(Path(runner_dir).parent.absolute())
     sys.path.append(path)
     import pymock_api.cmd
+    import pymock_api.cmd_ps
     from pymock_api._utils.file_opt import YAML
     from pymock_api.model import (
         ParserArguments,
@@ -38,25 +40,8 @@ class CommandRunner:
         self.cmd_parser: ArgumentParser = self.mock_api_parser.parse()
         self._server_gateway: BaseSGIServer = None
 
-    # TODO: It may use responsibility chain to design and implement command line features
-    def run(self, args: ParserArguments) -> None:
-        pass
-
-    def run_app(self, args: SubcmdRunArguments) -> None:
-        self._process_option(args)
-        self._server_gateway.run(args)
-
-    def run_config(self, args: SubcmdConfigArguments) -> None:
-        yaml: YAML = None
-        sample_data: str = None
-        if args.print_sample or args.generate_sample:
-            yaml = YAML()
-            sample_data = yaml.serialize(config=Sample_Config_Value)
-        if args.print_sample:
-            print(f"It will write below content into file {args.sample_output_path}:")
-            print(f"{sample_data}")
-        if args.generate_sample:
-            yaml.write(path=args.sample_output_path, config=sample_data)
+    def run(self, cmd_args: ParserArguments) -> None:
+        pymock_api.cmd_ps.run_command_chain(cmd_args)
 
     def parse(self, cmd_args: Optional[List[str]] = None) -> Union[SubcmdRunArguments, SubcmdConfigArguments]:
         if self.mock_api_parser.subcommand == pymock_api.cmd.SubCommand.Run:
@@ -75,30 +60,11 @@ class CommandRunner:
     def _parse_cmd_arguments(self, cmd_args: Optional[List[str]] = None) -> Namespace:
         return self.cmd_parser.parse_args(cmd_args)
 
-    def _process_option(self, parser_options: SubcmdRunArguments) -> None:
-        # Note: It's possible that it should separate the functions to be multiple objects to implement and manage the
-        # behaviors of command line with different options.
-        # Handle *config*
-        os.environ["MockAPI_Config"] = parser_options.config
-
-        # Handle *app-type*
-        if re.search(r"flask", parser_options.app_type, re.IGNORECASE):
-            self._server_gateway = setup_wsgi()
-        elif re.search(r"fastapi", parser_options.app_type, re.IGNORECASE):
-            self._server_gateway = setup_asgi()
-        else:
-            raise ValueError("Invalid value at argument *app-type*. It only supports 'flask' or 'fastapi' currently.")
-
 
 def run() -> None:
     cmd_runner = CommandRunner()
     arguments = cmd_runner.parse()
-    if arguments.subparser_name == pymock_api.cmd.SubCommand.Run:
-        cmd_runner.run_app(arguments)
-    elif arguments.subparser_name == pymock_api.cmd.SubCommand.Config:
-        cmd_runner.run_config(arguments)
-    else:
-        cmd_runner.run(arguments)
+    cmd_runner.run(arguments)
 
 
 if __name__ == "__main__":
