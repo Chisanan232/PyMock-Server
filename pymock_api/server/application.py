a3f1e02ec51c317b94f7220e64a5d7a2a75b2e24
Chisanan232 <chi10211201@cycu.org.tw>
2023-05-16 17:53:25 +0800

    Implement the argument checking feature with FastAPI web framework.

--- pymock_api/server/application.py
+++ pymock_api/server/application.py
@@ -52,6 +52,7 @@ class BaseAppServer(metaclass=ABCMeta):
                 add_api_pycode = self._add_api(
                     api_name, api_config, base_url=mocked_apis.base.url if mocked_apis.base else None
                 )
+                print(f"[DEBUG in src] annotate_function_pycode: {annotate_function_pycode}")
                 # pylint: disable=exec-used
                 exec(annotate_function_pycode)
                 # pylint: disable=exec-used
@@ -67,12 +68,46 @@ class BaseAppServer(metaclass=ABCMeta):
         """
         return initial_global_server + define_function_for_api
 
+    def _request_process(self, **kwargs) -> "flask.Response":  # type: ignore
+        print(f"[DEBUG in src] kwargs: {kwargs}")
+        request = self._get_current_request(**kwargs)
+        req_params = self._get_api_current_params_info(**kwargs)
+
+        print(f"[DEBUG in src] request: {request}")
+        print(f"[DEBUG in src] req_params: {req_params}")
+
+        api_params_info: List[APIParameter] = self._api_params[self._get_current_api_path(request)].http.request.parameters  # type: ignore[union-attr]
+        for param_info in api_params_info:
+            if param_info.required and param_info.name not in req_params.keys():
+                return self._generate_http_response(f"Miss required parameter *{param_info.name}*.", status_code=400)
+            one_req_param_value = req_params.get(param_info.name, None)
+            if one_req_param_value:
+                if param_info.value_type and not isinstance(one_req_param_value, locate(param_info.value_type)):  # type: ignore[arg-type]
+                    return self._generate_http_response(
+                        f"The type of data from Font-End site (*{type(one_req_param_value)}*) is different with the "
+                        f"implementation of Back-End site (*{type(param_info.value_type)}*).",
+                        status_code=400,
+                    )
+                if param_info.value_format and not re.search(
+                    param_info.value_format, one_req_param_value, re.IGNORECASE
+                ):
+                    return self._generate_http_response(
+                        f"The format of data from Font-End site (value: *{one_req_param_value}*) is incorrect. Its "
+                        f"format should be '{param_info.value_format}'.",
+                        status_code=400,
+                    )
+        return self._generate_http_response(body="OK.", status_code=200)
+
     @abstractmethod
-    def _request_process(self) -> None:
+    def _get_current_api_path(self, request: Any) -> str:
         pass
 
     @abstractmethod
-    def _get_current_request(self) -> Any:
+    def _get_current_request(self, **kwargs) -> Any:
+        pass
+
+    @abstractmethod
+    def _get_api_current_params_info(self, **kwargs) -> dict:
         pass
 
     def _ensure_http(self, api_config: MockAPI, http_attr: str) -> Union[HTTPRequest, HTTPResponse]:
@@ -103,34 +138,44 @@ class FlaskServer(BaseAppServer):
     def setup(self) -> "flask.Flask":  # type: ignore
         return import_web_lib.flask().Flask(__name__)
 
-    def _request_process(self) -> "flask.Response":  # type: ignore
-        request = self._get_current_request()
-        req_params = request.args if request.method.upper() == "GET" else request.form or request.data or request.json
+    # def _request_process(self, **kwargs) -> "flask.Response":  # type: ignore
+    #     request = self._get_current_request(**kwargs)
+    #     req_params = self._get_api_current_parms_info(request, **kwargs)
+    #
+    #     api_params_info: List[APIParameter] = self._api_params[request.path].http.request.parameters  # type: ignore[union-attr]
+    #     for param_info in api_params_info:
+    #         if param_info.required and param_info.name not in req_params.keys():
+    #             return self._generate_http_response(f"Miss required parameter *{param_info.name}*.", status_code=400)
+    #         one_req_param_value = req_params.get(param_info.name, None)
+    #         if one_req_param_value:
+    #             if param_info.value_type and not isinstance(one_req_param_value, locate(param_info.value_type)):  # type: ignore[arg-type]
+    #                 return self._generate_http_response(
+    #                     f"The type of data from Font-End site (*{type(one_req_param_value)}*) is different with the "
+    #                     f"implementation of Back-End site (*{type(param_info.value_type)}*).",
+    #                     status_code=400,
+    #                 )
+    #             if param_info.value_format and not re.search(
+    #                 param_info.value_format, one_req_param_value, re.IGNORECASE
+    #             ):
+    #                 return self._generate_http_response(
+    #                     f"The format of data from Font-End site (value: *{one_req_param_value}*) is incorrect. Its "
+    #                     f"format should be '{param_info.value_format}'.",
+    #                     status_code=400,
+    #                 )
+    #     return self._generate_http_response(body="OK.", status_code=200)
 
-        api_params_info: List[APIParameter] = self._api_params[request.path].http.request.parameters  # type: ignore[union-attr]
-        for param_info in api_params_info:
-            if param_info.required and param_info.name not in req_params:
-                return self._generate_http_response(f"Miss required parameter *{param_info.name}*.", status_code=400)
-            one_req_param_value = req_params.get(param_info.name, None)
-            if one_req_param_value:
-                if param_info.value_type and not isinstance(one_req_param_value, locate(param_info.value_type)):  # type: ignore[arg-type]
-                    return self._generate_http_response(
-                        f"The type of data from Font-End site (*{type(one_req_param_value)}*) is different with the "
-                        f"implementation of Back-End site (*{type(param_info.value_type)}*).",
-                        status_code=400,
-                    )
-                if param_info.value_format and not re.search(
-                    param_info.value_format, one_req_param_value, re.IGNORECASE
-                ):
-                    return self._generate_http_response(
-                        f"The format of data from Font-End site (value: *{one_req_param_value}*) is incorrect. Its "
-                        f"format should be '{param_info.value_format}'.",
-                        status_code=400,
-                    )
-        return self._generate_http_response(body="OK.", status_code=200)
-
-    def _get_current_request(self) -> "flask.Request":  # type: ignore
+    def _get_current_request(self, **kwargs) -> "flask.Request":  # type: ignore
         return import_web_lib.flask().request
+
+    def _get_current_api_path(self, request: "flask.Request") -> str:  # type: ignore
+        return request.path
+
+    def _get_api_current_params_info(self, **kwargs) -> dict:
+        request: "flask.Request" = kwargs.get("request", import_web_lib.flask().request)  # type: ignore
+        api_params = request.args if request.method.upper() == "GET" else request.form or request.data or request.json
+        if isinstance(api_params, bytes):
+            api_params = json.loads(api_params.decode("utf-8"))
+        return api_params
 
     def _generate_http_response(self, body: str, status_code: int) -> "flask.Response":  # type: ignore
         return import_web_lib.flask().Response(body, status=status_code)
@@ -149,14 +194,49 @@ class FastAPIServer(BaseAppServer):
     def setup(self) -> "fastapi.FastAPI":  # type: ignore
         return import_web_lib.fastapi().FastAPI()
 
-    def _request_process(self) -> "fastapi.Response":  # type: ignore
-        print(f"[DEBUG in src] Here is request process by FastAPI.")
-        # FIXME: Implement the request process in FastAPI strategy
-        request = self._get_current_request()
-        return self._generate_http_response("OK.", status_code=200)
+    def _annotate_function(self, api_name: str, api_config: MockAPI) -> str:
+        initial_global_server = f"""global SERVER\nSERVER = self\n"""
+        new_api_name = "".join(map(lambda n: f"{n[0].upper()}{n[1:]}", api_name.split("_")))
+        parameter_class = f"{new_api_name}Parameter"
+        define_parameters_model = f"""from pydantic import BaseModel\nclass {parameter_class}(BaseModel):\n"""
+        for prop in api_config.http.request.parameters:  # type: ignore[union-attr]
+            if prop.default:
+                define_parameters_model += f"    {prop.name}: {prop.value_type} = '{prop.default}'\n"
+            else:
+                define_parameters_model += f"    {prop.name}: {prop.value_type}\n"
+        import_fastapi = "from fastapi import Request as FastAPIRequest\n"
+        define_function_for_api = f"""def {api_name}(model: {parameter_class}, request: FastAPIRequest):
+            process_result = SERVER._request_process(model=model, request=request)
+            if process_result.status_code != 200:
+                return process_result
+            return _HTTPResponse.generate(data='{cast(HTTPResponse, self._ensure_http(api_config, "response")).value}')
+        """
+        return import_fastapi + initial_global_server + define_parameters_model + define_function_for_api
 
-    def _get_current_request(self) -> "fastapi.Request":  # type: ignore
-        return None
+    # def _request_process(self, **kwargs) -> "fastapi.Response":  # type: ignore
+    #     print(f"[DEBUG in src] Here is request process by FastAPI.")
+    #     # FIXME: Implement the request process in FastAPI strategy
+    #     request: "fastapi.Request" = self._get_current_request(**kwargs)
+    #     api_params_info: List[APIParameter] = self._api_params[request.scope['root_path'] + request.scope['route'].path].http.request.parameters  # type: ignore[union-attr]
+    #     for param_info in api_params_info:
+    #         if param_info.required and param_info.name not in kwargs.keys():
+    #             return self._generate_http_response(f"Miss required parameter *{param_info.name}*.", status_code=400)
+    #     return self._generate_http_response("OK.", status_code=200)
+
+    def _get_current_request(self, **kwargs) -> "fastapi.Request":  # type: ignore[name-defined]
+        return kwargs.get("request")
+
+    def _get_current_api_path(self, request: "fastapi.Request") -> str:  # type: ignore[name-defined]
+        print(f"[DEBUG in src] request: {request}")
+        return request.scope["root_path"] + request.scope["route"].path
+
+    def _get_api_current_params_info(self, **kwargs) -> dict:
+        api_params_info: List[APIParameter] = self._api_params[self._get_current_api_path(kwargs["request"])].http.request.parameters  # type: ignore[union-attr]
+        api_param_names = list(map(lambda e: e.name, api_params_info))
+        api_param = {}
+        for param_name in api_param_names:
+            api_param[param_name] = getattr(kwargs["model"], param_name)
+        return api_param
 
     def _generate_http_response(self, body: str, status_code: int) -> "fastapi.Response":  # type: ignore
         return import_web_lib.fastapi().Response(body, status_code=status_code)
