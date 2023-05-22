edd8e75192ec3bb310ac8e65f4010dbcffff2876
Chisanan232 <chi10211201@cycu.org.tw>
2023-05-22 18:30:40 +0800

    Add implementing generates Python code when API without any parameters.

--- pymock_api/server/application.py
+++ pymock_api/server/application.py
@@ -216,8 +216,10 @@ class FastAPIServer(BaseAppServer):
 
     def _define_api_function_pycode(self, api_name: str, api_config: MockAPI) -> str:
         if api_config.http.request.method.upper() != "GET":  # type: ignore[union-attr]
-            parameter_class = self._api_name_as_camel_case(api_name)
-            api_func_signature = f"model: {parameter_class}, " if self._api_has_params else ""
+            api_func_signature = ""
+            if api_config.http.request.parameters:  # type: ignore[union-attr]
+                parameter_class = self._api_name_as_camel_case(api_name)
+                api_func_signature = f"model: {parameter_class}, " if self._api_has_params else ""
             return f"""def {api_name}({api_func_signature}request: FastAPIRequest):
                 {self._run_request_process_pycode()}
                 {self._handle_request_process_result_pycode()}
@@ -226,8 +228,9 @@ class FastAPIServer(BaseAppServer):
         else:
             function_args = ""
             assign_value_to_model = ""
+            instantiate_model = ""
             for param in api_config.http.request.parameters:  # type: ignore[union-attr]
-                if param.default:
+                if param.default is not None:
                     if param.value_type == "str":
                         default_value = f"'{param.default}'"
                     else:
@@ -238,11 +241,12 @@ class FastAPIServer(BaseAppServer):
                 assign_value_to_model += f"""
         setattr(model, '{param.name}', {param.name})
                 """
-            parameter_class = self._api_name_as_camel_case(api_name)
-            instantiate_model = f"""
+            if api_config.http.request.parameters:  # type: ignore[union-attr]
+                parameter_class = self._api_name_as_camel_case(api_name)
+                instantiate_model = f"""
         class {parameter_class}: pass
         model = {parameter_class}()
-            """
+                """
             return f"""def {api_name}(request: FastAPIRequest{function_args}):
                 {instantiate_model}
                 {assign_value_to_model}
@@ -266,7 +270,7 @@ class FastAPIServer(BaseAppServer):
                 parameter_class = self._api_name_as_camel_case(api_name)
                 define_parameters_model = f"""from pydantic import BaseModel\nclass {parameter_class}(BaseModel):\n"""
             for prop in api_config.http.request.parameters:  # type: ignore[union-attr]
-                if prop.default:
+                if prop.default is not None:
                     define_parameters_model += f"    {prop.name}: {prop.value_type} = '{prop.default}'\n"
                 else:
                     if prop.required:
@@ -274,7 +278,9 @@ class FastAPIServer(BaseAppServer):
                     else:
                         define_parameters_model += f"    {prop.name}: {prop.value_type} = None\n"
             return define_parameters_model
-        return define_parameters_model
+        else:
+            self._api_has_params = False
+            return define_parameters_model
 
     def _run_request_process_pycode(self, **kwargs) -> str:
         return (
