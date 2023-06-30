cf1e04849caeafc1f9d27b765df672aeafce57bb
Chisanan232 <chi10211201@cycu.org.tw>
2023-06-30 10:15:30 +0800

    🧬 Refactor to use flag way to get argument by shell script.

--- scripts/docker/build.sh
+++ scripts/docker/build.sh
@@ -16,11 +16,54 @@
 # Check the environment variables and set default value to them if it's empty
 echo "Start to check command line tool arguments ..."
 
-Input_Arg_Release_Type=$1
-Input_Arg_Python_Pkg_Name=$2
-Input_Arg_Software_Version_Format=$3
-Docker_Image_Name=$4
-Running_Mode=$5
+while getopts "r:p:v:i:d:h:?" argv
+do
+     case $argv in
+         "r")    # Release type
+           Input_Arg_Release_Type=$OPTARG
+           ;;
+         "p")    # Python package name
+           Input_Arg_Python_Pkg_Name=$OPTARG
+           ;;
+         "v")    # Software version format
+           Input_Arg_Software_Version_Format=$OPTARG
+           ;;
+         "i")    # Use this to name Docker image it will build
+           Docker_Image_Name=$OPTARG
+           ;;
+         "d")    # Dry run
+           Running_Mode=$OPTARG
+           ;;
+         "h")    # Help for display all usage of each arguments
+           echo "Shell script usage: bash ./scripts/docker/build.sh [OPTION] [VALUE]"
+           echo " "
+           echo "This is a shell script for building Docker image with software version which be get from package info module (__pkg_info__) from Python package."
+           echo " "
+           echo "options:"
+           if [ "$OPTARG" == "r" ] || [ "$OPTARG" == "h" ] || [ "$OPTARG" == "all" ]; then
+               echo "  -r [Release type]              Release type of project. Different release type it would get different version format. [options: python-package]"
+           fi
+           if [ "$OPTARG" == "p" ] || [ "$OPTARG" == "h" ] || [ "$OPTARG" == "all" ]; then
+               echo "  -p [Python package name]       The Python package name. It will use this naming to get the package info module (__pkg_info__.py) to get the version info."
+           fi
+           if [ "$OPTARG" == "v" ] || [ "$OPTARG" == "h" ] || [ "$OPTARG" == "all" ]; then
+               echo "  -v [Version format]            Which version format you should use. [options: general-2, general-3, date-based]"
+           fi
+           if [ "$OPTARG" == "i" ] || [ "$OPTARG" == "h" ] || [ "$OPTARG" == "all" ]; then
+               echo "  -i [Docker image name]         Set the naming to the Docker image this shell will build."
+           fi
+           if [ "$OPTARG" == "d" ] || [ "$OPTARG" == "h" ] || [ "$OPTARG" == "all" ]; then
+               echo "  -d [Run mode]                  Running mode. Set 'dry-run' or 'debug' to let it only show log message without exactly working. [options: general, dry-run, debug]"
+           fi
+           echo "  -h [Argument]                  Show this help. You could set a specific argument naming to show the option usage. Empty or 'all' would show all arguments usage. [options: r, p, v, i, d, h]"
+           exit
+           ;;
+         ?)
+           echo "Invalid command line argument. Please use option *h* to get more details of argument usage."
+           exit
+           ;;
+     esac
+done
 
 # # # # From the PEP440: Software version style rule
 # # #
