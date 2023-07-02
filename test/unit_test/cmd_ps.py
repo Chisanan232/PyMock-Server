43815b63fd3bf69e758dc075c6e766e42d860a97
Chisanan232 <chi10211201@cycu.org.tw>
2023-07-02 20:42:24 +0800

    @pre-commit-ci auto-fix by pre-commit.

--- test/unit_test/cmd_ps.py
+++ test/unit_test/cmd_ps.py
@@ -582,7 +582,9 @@ RESPONSE_JSON_PATHS_WITH_EX_CODE: List[tuple] = []
 
 def _get_all_json(has_base_info: bool, config_type: str, exit_code: Union[str, int]) -> None:
     file_naming = "has-base-info" if has_base_info else "no-base-info"
-    json_dir = os.path.join(str(pathlib.Path(__file__).parent.parent), "data", "inspect_test", "api_response", f"{file_naming}*.json")
+    json_dir = os.path.join(
+        str(pathlib.Path(__file__).parent.parent), "data", "inspect_test", "api_response", f"{file_naming}*.json"
+    )
     global RESPONSE_JSON_PATHS_WITH_EX_CODE
     for json_config_path in glob.glob(json_dir):
         yaml_file_format = "*.yaml" if config_type == "invalid" else f"{file_naming}*.yaml"
