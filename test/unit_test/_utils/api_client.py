ae4ca068afbc01065fc18fb39af4faf0c371dc36
Chisanan232 <chi10211201@cycu.org.tw>
2023-07-11 08:52:50 +0800

    🧬 Move the test data to directory 'test/data/check_test/diff_with_swagger' to be more clear.

--- test/unit_test/_utils/api_client.py
+++ test/unit_test/_utils/api_client.py
@@ -15,7 +15,12 @@ RESPONSE_JSON_PATHS: List[str] = []
 
 def _get_all_swagger_config() -> None:
     json_dir = os.path.join(
-        str(pathlib.Path(__file__).parent.parent.parent), "data", "inspect_test", "api_response", "*.json"
+        str(pathlib.Path(__file__).parent.parent.parent),
+        "data",
+        "check_test",
+        "diff_with_swagger",
+        "api_response",
+        "*.json",
     )
     global RESPONSE_JSON_PATHS
     for json_config_path in glob.glob(json_dir):
