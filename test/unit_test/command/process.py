118f4820bee707f85b265522bc036cedf8549c32
Chisanan232 <chi10211201@cycu.org.tw>
2023-07-15 13:18:51 +0800

    🧬 Move the tests to new test module in new test sub-package.

--- test/unit_test/command/process.py
+++ test/unit_test/command/process.py
@@ -522,61 +522,6 @@ _get_all_yaml(config_type="valid", exit_code=0)
 _get_all_yaml(config_type="invalid", exit_code=1)
 
 
-RESPONSE_JSON_PATHS_WITH_EX_CODE: List[tuple] = []
-RESPONSE_JSON_PATHS: List[str] = []
-
-
-def _get_all_json(has_base_info: bool, config_type: str, exit_code: Union[str, int]) -> None:
-    file_naming = "has-base-info" if has_base_info else "no-base-info"
-    json_dir = os.path.join(
-        str(pathlib.Path(__file__).parent.parent.parent),
-        "data",
-        "check_test",
-        "diff_with_swagger",
-        "api_response",
-        f"{file_naming}*.json",
-    )
-    global RESPONSE_JSON_PATHS_WITH_EX_CODE
-    for json_config_path in glob.glob(json_dir):
-        yaml_file_format = "*.yaml" if config_type == "invalid" else f"{file_naming}*.yaml"
-        yaml_dir = os.path.join(
-            str(pathlib.Path(__file__).parent.parent.parent),
-            "data",
-            "check_test",
-            "diff_with_swagger",
-            "config",
-            config_type,
-            yaml_file_format,
-        )
-        expected_exit_code = exit_code if isinstance(exit_code, str) and exit_code.isdigit() else str(exit_code)
-        for yaml_config_path in glob.glob(yaml_dir):
-            for stop_if_fail in (True, False):
-                one_test_scenario = (json_config_path, yaml_config_path, stop_if_fail, expected_exit_code)
-                RESPONSE_JSON_PATHS_WITH_EX_CODE.append(one_test_scenario)
-
-
-def _get_all_swagger_config() -> None:
-    json_dir = os.path.join(
-        str(pathlib.Path(__file__).parent.parent.parent),
-        "data",
-        "check_test",
-        "diff_with_swagger",
-        "api_response",
-        "*.json",
-    )
-    global RESPONSE_JSON_PATHS
-    for json_config_path in glob.glob(json_dir):
-        one_test_scenario = json_config_path
-        RESPONSE_JSON_PATHS.append(one_test_scenario)
-
-
-_get_all_json(has_base_info=False, config_type="valid", exit_code=0)
-_get_all_json(has_base_info=True, config_type="valid", exit_code=0)
-_get_all_json(has_base_info=False, config_type="invalid", exit_code=1)
-
-_get_all_swagger_config()
-
-
 class TestSubCmdCheck(BaseCommandProcessorTestSpec):
     @pytest.fixture(scope="function")
     def cmd_ps(self) -> SubCmdCheck:
@@ -621,66 +566,6 @@ class TestSubCmdCheck(BaseCommandProcessorTestSpec):
         assert expected_exit_code in str(exc_info.value)
         # TODO: Add one more checking of the error message content with function *_expected_err_msg*
 
-    @pytest.mark.parametrize(
-        ("api_resp_path", "dummy_yaml_path", "stop_if_fail", "expected_exit_code"),
-        RESPONSE_JSON_PATHS_WITH_EX_CODE,
-    )
-    def test_with_command_processor_of_diff_swagger(
-        self,
-        api_resp_path: str,
-        dummy_yaml_path: str,
-        stop_if_fail: bool,
-        expected_exit_code: str,
-        object_under_test: Callable,
-    ):
-        kwargs = {
-            "api_resp_path": api_resp_path,
-            "dummy_yaml_path": dummy_yaml_path,
-            "stop_if_fail": stop_if_fail,
-            "expected_exit_code": expected_exit_code,
-            "cmd_ps": object_under_test,
-        }
-        self._test_process_of_diff_swagger(**kwargs)
-
-    @pytest.mark.parametrize(
-        ("api_resp_path", "dummy_yaml_path", "stop_if_fail", "expected_exit_code"),
-        RESPONSE_JSON_PATHS_WITH_EX_CODE,
-    )
-    def test_with_run_entry_point_of_diff_swagger(
-        self,
-        api_resp_path: str,
-        dummy_yaml_path: str,
-        stop_if_fail: bool,
-        expected_exit_code: str,
-        entry_point_under_test: Callable,
-    ):
-        kwargs = {
-            "api_resp_path": api_resp_path,
-            "dummy_yaml_path": dummy_yaml_path,
-            "stop_if_fail": stop_if_fail,
-            "expected_exit_code": expected_exit_code,
-            "cmd_ps": entry_point_under_test,
-        }
-        self._test_process_of_diff_swagger(**kwargs)
-
-    def _test_process_of_diff_swagger(
-        self, api_resp_path: str, dummy_yaml_path: str, expected_exit_code: str, cmd_ps: Callable, stop_if_fail: bool
-    ):
-        mock_parser_arg = _given_parser_args(
-            subcommand=_Test_SubCommand_Check, swagger_doc_url=_Swagger_API_Document_URL, stop_if_fail=stop_if_fail
-        )
-        with patch("pymock_api.command.process.load_config") as mock_load_config:
-            mock_load_config.return_value = load_config(dummy_yaml_path)
-            with patch.object(SubCmdCheck, "_get_swagger_config") as mock_get_swagger_config:
-                with open(api_resp_path, "r", encoding="utf-8") as file_stream:
-                    mock_get_swagger_config.return_value = deserialize_swagger_api_config(
-                        json.loads(file_stream.read())
-                    )
-
-                with pytest.raises(SystemExit) as exc_info:
-                    cmd_ps(mock_parser_arg)
-                assert expected_exit_code in str(exc_info.value)
-
     def _given_cmd_args_namespace(self) -> Namespace:
         args_namespace = Namespace()
         args_namespace.subcommand = SubCommand.Check
@@ -697,29 +582,6 @@ class TestSubCmdCheck(BaseCommandProcessorTestSpec):
 
     def _expected_argument_type(self) -> Type[SubcmdCheckArguments]:
         return SubcmdCheckArguments
-
-    def test__setting_should_be_valid(self, cmd_ps: SubCmdCheck):
-        test_callback = MagicMock()
-        cmd_ps._setting_should_be_valid(
-            config_key="key", config_value="value", criteria=["value"], valid_callback=test_callback
-        )
-        test_callback.assert_called_once_with("key", "value", ["value"])
-
-    def test__setting_should_be_valid_with_invalid_type_criteria(self, cmd_ps: SubCmdCheck):
-        with pytest.raises(TypeError) as exc_info:
-            cmd_ps._setting_should_be_valid(
-                config_key="any key", config_value="any value", criteria="invalid type value"
-            )
-        assert re.search(r"only accept 'list'", str(exc_info.value), re.IGNORECASE)
-
-    @pytest.mark.parametrize("swagger_config_response", RESPONSE_JSON_PATHS)
-    def test__get_swagger_config(self, swagger_config_response: str, cmd_ps: SubCmdCheck):
-        with patch("pymock_api.command.process.URLLibHTTPClient.request") as mock_api_client_request:
-            with open(swagger_config_response, "r", encoding="utf-8") as file_stream:
-                mock_api_client_request.return_value = json.loads(file_stream.read())
-
-            cmd_ps._get_swagger_config(_Swagger_API_Document_URL)
-            mock_api_client_request.assert_called_once_with(method="GET", url=_Swagger_API_Document_URL)
 
 
 class TestSubCmdInspect(BaseCommandProcessorTestSpec):
