02910a585487b0c36171f93ece78552578f4da40
Chisanan232 <chi10211201@cycu.org.tw>
2023-07-22 16:24:22 +0800

    🧬 Extract the common logic of entire test suite as template test suite.

--- test/integration_test/_utils/file_opt.py
+++ test/integration_test/_utils/file_opt.py
@@ -1,4 +1,5 @@
 import os
+from abc import ABCMeta, abstractmethod
 
 import pytest
 
@@ -7,38 +8,42 @@ try:
 except ImportError:
     from yaml import Dumper  # type: ignore
 
-from pymock_api._utils.file_opt import YAML
+from pymock_api._utils.file_opt import YAML, _BaseFileOperation
 
 from ..._values import _Test_Config_Value
 from .._spec import MockAPI_Config_Path, run_test
 
 
-class TestYAML:
+class _BaseTestSuite(metaclass=ABCMeta):
     _Test_File: str = "pytest-yaml-write.yaml"
 
     @pytest.fixture(scope="function")
-    def yaml_opt(self) -> YAML:
+    def file_opt(self) -> _BaseFileOperation:
         self._remove_test_config()
-        return YAML()
+        return self._under_test_object()
+
+    @abstractmethod
+    def _under_test_object(self) -> _BaseFileOperation:
+        pass
 
     @run_test.with_file
-    def test_open_and_read(self, yaml_opt: YAML):
+    def test_open_and_read(self, file_opt: _BaseFileOperation):
         # Run target function
-        reading_data = yaml_opt.read(path=MockAPI_Config_Path)
+        reading_data = file_opt.read(path=MockAPI_Config_Path)
 
         # Verify result
         assert reading_data and isinstance(reading_data, dict)
         assert reading_data == _Test_Config_Value
 
-    def test_write(self, yaml_opt: YAML):
+    def test_write(self, file_opt: _BaseFileOperation):
         try:
             assert os.path.exists(self._Test_File) is False
-            config_data = yaml_opt.serialize(config=_Test_Config_Value)
-            yaml_opt.write(path=self._Test_File, config=config_data)
+            config_data = file_opt.serialize(config=_Test_Config_Value)
+            file_opt.write(path=self._Test_File, config=config_data)
 
             assert isinstance(config_data, str)
             assert os.path.exists(self._Test_File)
-            config = yaml_opt.read(self._Test_File)
+            config = file_opt.read(self._Test_File)
             assert config == _Test_Config_Value
         finally:
             self._remove_test_config()
@@ -46,3 +51,8 @@ class TestYAML:
     def _remove_test_config(self):
         if os.path.exists(self._Test_File):
             os.remove(self._Test_File)
+
+
+class TestYAML(_BaseTestSuite):
+    def _under_test_object(self) -> YAML:
+        return YAML()
