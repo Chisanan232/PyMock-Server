aa7509dcf0bd8d855044f8b4443082aeff8be64e
Chisanan232 <chi10211201@cycu.org.tw>
2023-07-24 00:15:08 +0800

    ⚙️ Add new unit test for new file operator (still have problem needs to fix).

--- test/unit_test/_utils/file_opt.py
+++ test/unit_test/_utils/file_opt.py
@@ -1,3 +1,4 @@
+import json
 from abc import ABCMeta, abstractmethod
 from unittest.mock import MagicMock, mock_open, patch
 
@@ -9,7 +10,7 @@ try:
 except ImportError:
     from yaml import Loader
 
-from pymock_api._utils.file_opt import YAML, _BaseFileOperation
+from pymock_api._utils.file_opt import JSON, YAML, _BaseFileOperation
 
 
 class _FileOptTestSpec(metaclass=ABCMeta):
@@ -19,8 +20,9 @@ class _FileOptTestSpec(metaclass=ABCMeta):
         pass
 
     @property
+    @abstractmethod
     def not_exist_file(self) -> str:
-        return "file_not_found.yaml"
+        pass
 
     def test_read_with_not_exist_file(self, file_opt: _BaseFileOperation):
         # Mock functions
@@ -29,11 +31,11 @@ class _FileOptTestSpec(metaclass=ABCMeta):
             with pytest.raises(FileNotFoundError) as exc_info:
                 # Run target function to test
                 file_opt.read(path=self.not_exist_file)
-                # Verify result
-                expected_err_msg = f"The target configuration file {self.not_exist_file} doesn't exist."
-                assert str(exc_info) == expected_err_msg, f"The error message should be same as '{expected_err_msg}'."
-                mock_file_stream.assert_not_called()
-                self._should_call_load()
+            # Verify result
+            expected_err_msg = f"The target configuration file {self.not_exist_file} doesn't exist."
+            assert str(exc_info.value) == expected_err_msg, f"The error message should be same as '{expected_err_msg}'."
+            mock_file_stream.assert_not_called()
+            self._should_call_load()
 
     @abstractmethod
     def _mock_load(self) -> None:
@@ -49,9 +51,30 @@ class TestYAML(_FileOptTestSpec):
     def file_opt(self) -> YAML:
         return YAML()
 
+    @property
+    def not_exist_file(self) -> str:
+        return "file_not_found.yaml"
+
     def _mock_load(self) -> None:
         yaml.load = MagicMock(return_value=None)
 
     def _should_call_load(self) -> None:
         assert isinstance(yaml.load, MagicMock), "The function should be annotated as *MagicMock* type."
         yaml.load.assert_not_called()
+
+
+class TestJSON(_FileOptTestSpec):
+    @pytest.fixture(scope="function")
+    def file_opt(self) -> JSON:
+        return JSON()
+
+    @property
+    def not_exist_file(self) -> str:
+        return "file_not_found.json"
+
+    def _mock_load(self) -> None:
+        json.loads = MagicMock(return_value=None)
+
+    def _should_call_load(self) -> None:
+        assert isinstance(json.loads, MagicMock), "The function should be annotated as *MagicMock* type."
+        json.loads.assert_not_called()
