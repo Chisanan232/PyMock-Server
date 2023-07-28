11ea81047550eb1cecc7683af587917675b28f78
Chisanan232 <chi10211201@cycu.org.tw>
2023-07-28 12:41:08 +0800

    ➕ Add TODO item for it could support JSON file.

--- pymock_api/command/add/component.py
+++ pymock_api/command/add/component.py
@@ -15,6 +15,7 @@ def _option_cannot_be_empty_assertion(cmd_option: str) -> str:
 
 class SubCmdAddComponent(BaseSubCmdComponent):
     def process(self, args: SubcmdAddArguments) -> None:  # type: ignore[override]
+        # TODO: Add logic about using mapping file operation by the file extension.
         yaml: YAML = YAML()
         sample_data: str = yaml.serialize(config=Sample_Config_Value)
         if args.print_sample:
