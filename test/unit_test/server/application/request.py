d83f40d33379ad2ddc4cd904bff17ee374229eba
Chisanan232 <chi10211201@cycu.org.tw>
2023-08-26 09:28:28 +0800

    🧬 Pull the test as template test and extract the fixture object as abstract for different implementation.

--- test/unit_test/server/application/request.py
+++ test/unit_test/server/application/request.py
@@ -1,16 +1,34 @@
 import re
+from abc import ABCMeta, abstractmethod
 
 import pytest
 
-from pymock_api.server.application.request import FastAPIRequest
+from pymock_api.server.application.request import (
+    BaseCurrentRequest,
+    FastAPIRequest,
+    FlaskRequest,
+)
 
 
-class TestFastAPIRequest:
+class BaseCurrentRequestTestSpec(metaclass=ABCMeta):
     @pytest.fixture(scope="function")
-    def request_util(self) -> FastAPIRequest:
-        return FastAPIRequest()
+    @abstractmethod
+    def request_util(self) -> BaseCurrentRequest:
+        pass
 
     def test_api_parameters_with_missing_argument(self, request_util: FastAPIRequest):
         with pytest.raises(ValueError) as exc_info:
             request_util.api_parameters()
         assert re.search(r"missing .{0,128} argument", str(exc_info.value), re.IGNORECASE)
+
+
+class TestFlaskRequest(BaseCurrentRequestTestSpec):
+    @pytest.fixture(scope="function")
+    def request_util(self) -> FlaskRequest:
+        return FlaskRequest()
+
+
+class TestFastAPIRequest(BaseCurrentRequestTestSpec):
+    @pytest.fixture(scope="function")
+    def request_util(self) -> FastAPIRequest:
+        return FastAPIRequest()
