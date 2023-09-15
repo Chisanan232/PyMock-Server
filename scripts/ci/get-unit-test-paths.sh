f5b1b84d44019ddb1ae86f0f5c6adcb76eef33ae
Chisanan232 <chi10211201@cycu.org.tw>
2023-09-15 15:20:58 +0800

    ➕ Add new file path to let shell script to get unit test module.

--- scripts/ci/get-unit-test-paths.sh
+++ scripts/ci/get-unit-test-paths.sh
@@ -6,6 +6,7 @@ runtime_os=$1
 declare -a init_tests
 declare -a utils_tests
 declare -a model_tests
+declare -a model_api_config_tests
 declare -a server_tests
 declare -a server_application_tests
 declare -a server_sgi_tests
@@ -31,6 +32,11 @@ getalltests() {
         # shellcheck disable=SC2124
         # shellcheck disable=SC2178
         utils_tests=${alltestpaths[@]}
+    elif echo "$1" | grep -q "api_config";
+    then
+        # shellcheck disable=SC2124
+        # shellcheck disable=SC2178
+        model_api_config_tests=${alltestpaths[@]}
     elif echo "$1" | grep -q "model";
     then
         # shellcheck disable=SC2124
@@ -91,6 +97,7 @@ getalltests() {
 init_path=./test/unit_test/
 utils_path=./test/unit_test/_utils/
 model_path=./test/unit_test/model/
+model_api_config_path=./test/unit_test/model/api_config/
 server_path=./test/unit_test/server/
 server_application_path=./test/unit_test/server/application/
 server_sgi_path=./test/unit_test/server/sgi/
@@ -104,6 +111,7 @@ subcommand_sample_path=./test/unit_test/command/sample/
 getalltests $init_path
 getalltests $utils_path
 getalltests $model_path
+getalltests $model_api_config_path
 getalltests $server_path
 getalltests $server_application_path
 getalltests $server_sgi_path
@@ -114,7 +122,7 @@ getalltests $subcommand_check_path
 getalltests $subcommand_get_path
 getalltests $subcommand_sample_path
 
-dest=( "${init_tests[@]} ${utils_tests[@]} ${model_tests[@]} ${server_tests[@]} ${server_application_tests[@]} ${server_sgi_tests[@]} ${command_tests[@]} ${subcommand_run_tests[@]} ${subcommand_add_tests[@]} ${subcommand_check_tests[@]} ${subcommand_get_tests[@]} ${subcommand_sample_tests[@]}" )
+dest=( "${init_tests[@]} ${utils_tests[@]} ${model_tests[@]} ${model_api_config_tests[@]} ${server_tests[@]} ${server_application_tests[@]} ${server_sgi_tests[@]} ${command_tests[@]} ${subcommand_run_tests[@]} ${subcommand_add_tests[@]} ${subcommand_check_tests[@]} ${subcommand_get_tests[@]} ${subcommand_sample_tests[@]}" )
 
 if echo "$runtime_os" | grep -q "windows";
 then
