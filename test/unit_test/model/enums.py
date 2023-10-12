5f694069401cd46f2f84c84fc9fd051b5373f385
Chisanan232 <chi10211201@cycu.org.tw>
2023-10-12 15:44:30 +0800

    ➕ Add unit test for the function *to_enum* of enum object.

--- test/unit_test/model/enums.py
+++ test/unit_test/model/enums.py
@@ -1,8 +1,15 @@
 import re
+from abc import ABCMeta, abstractmethod
+from enum import Enum
+from typing import Any, Type
 
 import pytest
 
-from pymock_api.model.enums import set_loading_function
+from pymock_api.model.enums import (
+    ConfigLoadingOrder,
+    ResponseStrategy,
+    set_loading_function,
+)
 
 
 def test_set_loading_function():
@@ -11,3 +18,50 @@ def test_set_loading_function():
     assert re.search(
         r"The arguments only have \*apis\*, \*file\* and \*apply\*.{0,64}", str(exc_info.value), re.IGNORECASE
     )
+
+
+class EnumTestSuite(metaclass=ABCMeta):
+    @pytest.fixture(scope="function")
+    @abstractmethod
+    def enum_obj(self) -> Type[Enum]:
+        pass
+
+    def test_to_enum(self, value: Any, enum_obj: Type[Enum]):
+        result = enum_obj.to_enum(value)
+        assert isinstance(result, enum_obj)
+
+
+class TestResponseStrategy(EnumTestSuite):
+    @pytest.fixture(scope="function")
+    def enum_obj(self) -> Type[ResponseStrategy]:
+        return ResponseStrategy
+
+    @pytest.mark.parametrize(
+        "value",
+        [
+            ResponseStrategy.STRING,
+            ResponseStrategy.FILE,
+            "string",
+            "object",
+        ],
+    )
+    def test_to_enum(self, value: Any, enum_obj: Type[ResponseStrategy]):
+        super().test_to_enum(value, enum_obj)
+
+
+class TestConfigLoadingOrder(EnumTestSuite):
+    @pytest.fixture(scope="function")
+    def enum_obj(self) -> Type[ConfigLoadingOrder]:
+        return ConfigLoadingOrder
+
+    @pytest.mark.parametrize(
+        "value",
+        [
+            ConfigLoadingOrder.APIs,
+            ConfigLoadingOrder.APPLY,
+            "apis",
+            "file",
+        ],
+    )
+    def test_to_enum(self, value: Any, enum_obj: Type[ConfigLoadingOrder]):
+        super().test_to_enum(value, enum_obj)
