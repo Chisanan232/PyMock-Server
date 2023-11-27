421ee6130903766462da6454a1d6d0d95aac3dea
Chisanan232 <chi10211201@cycu.org.tw>
2023-11-27 21:29:14 +0800

    ✏️  Modify the value setting logic if it gets empty data of section *mocked_apis*.

--- pymock_api/model/api_config/__init__.py
+++ pymock_api/model/api_config/__init__.py
@@ -291,8 +291,11 @@ class MockAPIs(_Config, _Checkable, TemplateConfigLoadable, _Dividable):
         mock_api_config_name = os.path.basename(kwargs["path"]).replace(".yaml", "")
         format_rule_string = self._config_file_format.replace("**", "")
         mock_api_config_key = mock_api_config_name.replace(format_rule_string, "")
+        print(f"[DEBUG in MockAPIs._set_template_config] mock_api_config_key: {mock_api_config_key}")
+        print(f"[DEBUG in MockAPIs._set_template_config] self.apis: {self.apis}")
         # Set the data model in config
         self.apis[mock_api_config_key] = config
+        print(f"[DEBUG in MockAPIs._set_template_config] new self.apis: {self.apis}")
 
     def get_api_config_by_url(self, url: str, base: Optional[BaseConfig] = None) -> Optional[MockAPI]:
         url = url.replace(base.url, "") if base else url
@@ -500,14 +503,16 @@ class APIConfig(_Config, _Checkable):
         """
         self.name = data.get("name", None)
         self.description = data.get("description", None)
+
         mocked_apis = data.get("mocked_apis", None)
-        if mocked_apis:
-            mock_apis_data_model = MockAPIs()
-            mock_apis_data_model.set_template_in_config = self.set_template_in_config
-            mock_apis_data_model.config_file_name = self.config_file_name
-            mock_apis_data_model.absolute_model_key = self.key
-            mock_apis_data_model.divide_strategy = self._divide_strategy
-            self.apis = mock_apis_data_model.deserialize(data=mocked_apis)
+        mocked_apis = {"template": {}} if not mocked_apis else mocked_apis
+        mock_apis_data_model = MockAPIs()
+        mock_apis_data_model.set_template_in_config = self.set_template_in_config
+        mock_apis_data_model.config_file_name = self.config_file_name
+        mock_apis_data_model.absolute_model_key = self.key
+        mock_apis_data_model.divide_strategy = self._divide_strategy
+        self.apis = mock_apis_data_model.deserialize(data=mocked_apis)
+
         return self
 
     def is_work(self) -> bool:
