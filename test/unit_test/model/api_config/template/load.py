9085f1311c6e9d67b67fed8205d112864e59a9d9
Chisanan232 <chi10211201@cycu.org.tw>
2024-02-20 17:34:22 +0800

    ➕ Add test about checking the instance of template configuration operation should be different in each templatable data modals.

---
+++ test/unit_test/model/api_config/template/load.py
@@ -0,0 +1,48 @@
+import pytest
+
+from pymock_api.model.api_config import APIConfig
+from pymock_api.model.api_config.template._load import (
+    TemplatableConfigLoadable,
+    TemplateConfigOpts,
+    _BaseTemplateConfigLoader,
+)
+
+_Test_Data: str = "./test/data/check_test/data_model/entire_api/valid/has-base-info_and_tags.yaml"
+
+
+@pytest.fixture(scope="function")
+def api_config() -> APIConfig:
+    return APIConfig()
+
+
+def test_loader_independent(api_config: APIConfig):
+
+    def get_template_config_opts_id(_data_modal: TemplatableConfigLoadable) -> int:
+        assert hasattr(_data_modal, "_template_config_loader")
+        assert isinstance(_data_modal._template_config_loader, _BaseTemplateConfigLoader)
+
+        assert hasattr(_data_modal._template_config_loader, "_template_config_opts")
+        assert isinstance(_data_modal._template_config_loader._template_config_opts, TemplateConfigOpts)
+
+        return id(_data_modal._template_config_loader._template_config_opts)
+
+    # Deserialize a data modal *APIConfig*
+    api_config_data_modal = api_config.from_yaml(path=_Test_Data)
+
+    # Try to get other data modals which is templatable
+    # Verify their loader should include different instance of object *TemplateConfigOpts*
+    mocked_apis_modal = api_config_data_modal.apis
+    all_mocked_apis = mocked_apis_modal.apis
+    for key, api_modal in all_mocked_apis.items():
+        http_modal = api_modal.http
+
+        # Verify the data type
+        assert isinstance(mocked_apis_modal, TemplatableConfigLoadable)
+        assert isinstance(api_modal, TemplatableConfigLoadable)
+        assert isinstance(http_modal, TemplatableConfigLoadable)
+        # Verify the instance of template configuration operations
+        assert (
+            get_template_config_opts_id(mocked_apis_modal)
+            != get_template_config_opts_id(api_modal)
+            != get_template_config_opts_id(http_modal)
+        )
