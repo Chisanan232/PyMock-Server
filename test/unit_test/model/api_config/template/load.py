d6c4981049ec028f206e0524b205847478d1332c
Chisanan232 <chi10211201@cycu.org.tw>
2024-02-20 23:22:24 +0800

    🧬 Refactor the test to use parameterizing with different configuration.

--- test/unit_test/model/api_config/template/load.py
+++ test/unit_test/model/api_config/template/load.py
@@ -1,3 +1,8 @@
+import glob
+import os
+import pathlib
+from typing import List
+
 import pytest
 
 from pymock_api.model.api_config import APIConfig
@@ -7,7 +12,19 @@ from pymock_api.model.api_config.template._load import (
     _BaseTemplateConfigLoader,
 )
 
-_Test_Data: str = "./test/data/check_test/data_model/entire_api/valid/has-base-info_and_tags.yaml"
+# _Test_Data: str = "./test/data/check_test/data_model/entire_api/valid/has-base-info_and_tags.yaml"
+_Test_Data: List[str] = []
+yaml_dir = os.path.join(
+    str(pathlib.Path(__file__).parent.parent.parent.parent.parent),
+    "data",
+    "check_test",
+    "data_model",
+    "entire_api",
+    "valid",
+    "*.yaml",
+)
+for yaml_config_path in glob.glob(yaml_dir):
+    _Test_Data.append(yaml_config_path)
 
 
 @pytest.fixture(scope="function")
@@ -15,7 +32,8 @@ def api_config() -> APIConfig:
     return APIConfig()
 
 
-def test_loader_independent(api_config: APIConfig):
+@pytest.mark.parametrize("api_config_yaml_path", _Test_Data)
+def test_loader_independent(api_config: APIConfig, api_config_yaml_path: str):
 
     def get_template_config_opts_id(_data_modal: TemplatableConfigLoadable) -> int:
         assert hasattr(_data_modal, "_template_config_loader")
@@ -27,7 +45,7 @@ def test_loader_independent(api_config: APIConfig):
         return id(_data_modal._template_config_loader._template_config_opts)
 
     # Deserialize a data modal *APIConfig*
-    api_config_data_modal = api_config.from_yaml(path=_Test_Data)
+    api_config_data_modal = api_config.from_yaml(path=api_config_yaml_path)
 
     # Try to get other data modals which is templatable
     # Verify their loader should include different instance of object *TemplateConfigOpts*
