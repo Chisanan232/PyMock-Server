d6994516b738c5931c4bf32499f50fe7c5a9b5be
Chisanan232 <chi10211201@cycu.org.tw>
2024-03-25 21:02:34 +0800

    ✏️  Modify the details to get PR number via shell script.

--- scripts/ci/chk_pr_list.sh
+++ scripts/ci/chk_pr_list.sh
@@ -3,17 +3,19 @@
 set +e
 pr_info="$(gh pr list -l dependencies --search 'review:approved' --limit 1 | grep 'dependabot')"
 # shellcheck disable=SC2028
-echo "PR info: \n $pr_info"
+#echo "[DEBUG] PR info: \n $pr_info"
 pr_exist="$?"
 if [ "$pr_exist" == "1" ];
 then
-    echo "PR has been merged. Stop this CI workflow."
+#    echo "[DEBUG] PR has been merged. Stop this CI workflow."
     # Save the value to environment variable in GitHub Action
-    echo "PR_EXIST=$pr_exist" >> $GITHUB_OUTPUT
+#    echo "PR_EXIST=$pr_exist" >> $GITHUB_OUTPUT
+    exit 1
 else
     pr_number="$(echo "$pr_info" | cut -d ' ' -f 1 | tr -d -c 0-9)"
     # Save the value to environment variable in GitHub Action
-    echo "PR_NUMBER=$pr_number" >> $GITHUB_OUTPUT
-    # shellcheck disable=SC2153
-    echo "PR_NUMBER: $PR_NUMBER"
+#    echo "PR_NUMBER=$pr_number" >> $GITHUB_OUTPUT
+    # shellcheck disable=SC2059
+    printf "$pr_number"
+    exit 0
 fi
