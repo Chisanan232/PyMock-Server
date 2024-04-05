f091b251e644613859e992a3ce1573da9acf1b84
Chisanan232 <chi10211201@cycu.org.tw>
2024-04-05 09:34:28 +0800

    ✏️  Modify the test implementation to be more meaningful.

--- test/unit_test/model/openapi/inner_parser.py
+++ test/unit_test/model/openapi/inner_parser.py
@@ -1,15 +1,32 @@
 import json
 import os
 import re
+from typing import Dict, List, Type
 
 import pytest
 
-from pymock_api.model.openapi._parse import OpenAPIV2Parser
+from pymock_api.model.openapi._parse import BaseOpenAPIParser, OpenAPIV2Parser, OpenAPIV3Parser
+
+
+class DummyOpenAPIParser(BaseOpenAPIParser):
+    def get_paths(self) -> Dict[str, Dict]:
+        return {}
+
+    def get_tags(self) -> List[dict]:
+        return []
+
+    def get_objects(self) -> Dict[str, dict]:
+        return {}
 
 
 class TestOpenAPIParser:
 
-    def test_initial_parser_with_valid_file(self):
+    @pytest.fixture(scope="function")
+    def parser(self) -> Type[BaseOpenAPIParser]:
+        return DummyOpenAPIParser
+
+    @pytest.mark.parametrize("impl_parser", [OpenAPIV2Parser, OpenAPIV3Parser])
+    def test_initial_parser_with_valid_file(self, impl_parser: Type[BaseOpenAPIParser]):
         file_path: str = "./test.json"
         try:
             # Given
@@ -17,17 +34,17 @@ class TestOpenAPIParser:
                 io_stream.write(json.dumps({"paths": "test API"}))
 
             # Run target function
-            parser = OpenAPIV2Parser(file=file_path)
+            parser_instance = impl_parser(file=file_path)
 
             # Verify
-            assert parser.get_paths() == "test API"
+            assert parser_instance.get_paths() == "test API"
         finally:
             os.remove(file_path)
 
-    def test_initial_parser_with_invalid_file(self):
+    def test_initial_parser_with_invalid_file(self, parser: Type[BaseOpenAPIParser]):
         # Run target function
         with pytest.raises(FileNotFoundError) as exc_info:
-            OpenAPIV2Parser(file="./not_exist.json")
+            DummyOpenAPIParser(file="./not_exist.json")
 
         # Verify
         assert re.search(r".{0,32}not find.{0,32}OpenAPI format configuration", str(exc_info.value), re.IGNORECASE)
