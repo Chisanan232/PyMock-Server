e6a82e5bea247b143853840d1d05a14bd517ebdd
Chisanan232 <chi10211201@cycu.org.tw>
2024-04-10 15:17:34 +0800

    ➕ Add unit tests for the functions about generating response detail settings by enum object.

--- test/unit_test/model/enums.py
+++ test/unit_test/model/enums.py
@@ -11,6 +11,12 @@ from pymock_api.model.enums import (
     ResponseStrategy,
     set_loading_function,
 )
+from pymock_api.model.openapi._base import (
+    _YamlSchema,
+    ensure_get_schema_parser_factory,
+    set_component_definition,
+)
+from pymock_api.model.openapi._schema_parser import OpenAPIV3SchemaParser
 
 
 def test_set_loading_function():
@@ -48,6 +54,145 @@ class TestResponseStrategy(EnumTestSuite):
     )
     def test_to_enum(self, value: Any, enum_obj: Type[ResponseStrategy]):
         super().test_to_enum(value, enum_obj)
+
+    @pytest.mark.parametrize(
+        ("ut_enum", "expected_type"),
+        [
+            (ResponseStrategy.STRING, str),
+            (ResponseStrategy.FILE, str),
+            (ResponseStrategy.OBJECT, dict),
+        ],
+    )
+    def test_generate_empty_response(self, ut_enum: ResponseStrategy, expected_type: type):
+        empty_resp = ut_enum.generate_empty_response()
+        assert isinstance(empty_resp, expected_type)
+
+    @pytest.mark.parametrize(
+        ("ut_enum", "expected_type"),
+        [
+            (ResponseStrategy.STRING, str),
+            (ResponseStrategy.FILE, str),
+            (ResponseStrategy.OBJECT, dict),
+        ],
+    )
+    def test_generate_response_from_reference(self, ut_enum: ResponseStrategy, expected_type: type):
+        resp = ut_enum.generate_response_from_reference({"response reference data": {}})
+        assert isinstance(resp, expected_type)
+
+    @pytest.mark.parametrize(
+        ("ut_enum", "test_response_data", "expected_value"),
+        [
+            # # General data
+            (ResponseStrategy.STRING, {"type": "string"}, "random string value"),
+            (ResponseStrategy.FILE, {"type": "string"}, "random string value"),
+            (
+                ResponseStrategy.OBJECT,
+                {"type": "string"},
+                {"name": "", "required": True, "type": "str", "format": None, "items": None},
+            ),
+            # # Each different type as general data
+            # For string strategy (the process details of string and file are the same, so it only test one of them)
+            (ResponseStrategy.STRING, {"type": "integer"}, "random integer value"),
+            (ResponseStrategy.STRING, {"type": "number"}, "random integer value"),
+            (ResponseStrategy.STRING, {"type": "boolean"}, "random boolean value"),
+            (
+                ResponseStrategy.STRING,
+                {"type": "array", "items": {"$ref": "#/components/schemas/FooResponse"}},
+                [
+                    {
+                        "id": "random integer value",
+                        "name": "random string value",
+                        "value1": "random string value",
+                        "value2": "random string value",
+                    }
+                ],
+            ),
+            (ResponseStrategy.STRING, {"type": "file"}, "random file output stream"),
+            (ResponseStrategy.STRING, {"type": "object"}, "random object value"),
+            # For object strategy
+            (
+                ResponseStrategy.OBJECT,
+                {"type": "integer"},
+                {"name": "", "required": True, "type": "int", "format": None, "items": None},
+            ),
+            (
+                ResponseStrategy.OBJECT,
+                {"type": "number"},
+                {"name": "", "required": True, "type": "int", "format": None, "items": None},
+            ),
+            (
+                ResponseStrategy.OBJECT,
+                {"type": "boolean"},
+                {"name": "", "required": True, "type": "bool", "format": None, "items": None},
+            ),
+            (
+                ResponseStrategy.OBJECT,
+                {"type": "array", "items": {"$ref": "#/components/schemas/FooResponse"}},
+                {
+                    "name": "",
+                    "required": True,
+                    "type": "list",
+                    "format": None,
+                    "items": [
+                        {"name": "id", "required": True, "type": "int"},
+                        {"name": "name", "required": True, "type": "str"},
+                        {"name": "value1", "required": True, "type": "str"},
+                        {"name": "value2", "required": True, "type": "str"},
+                    ],
+                },
+            ),
+            (
+                ResponseStrategy.OBJECT,
+                {"type": "file"},
+                {"name": "", "required": True, "type": "file", "format": None, "items": None},
+            ),
+            (
+                ResponseStrategy.OBJECT,
+                {"type": "object"},
+                {"name": "", "required": True, "type": "dict", "format": None, "items": None},
+            ),
+            # # Special data
+            # (ResponseStrategy.STRING, {"type": "object", "additionalProperties": {"type": "array", "items": {"type": "string", "enum": ["IP_HOLDER", "PARTNER"]}}}, [{"id": "random integer value", "name": "random string value", "value1": "random string value", "value2": "random string value"}]),
+        ],
+    )
+    def test_generate_response_from_data(
+        self, ut_enum: ResponseStrategy, test_response_data: dict, expected_value: str
+    ):
+        # Pre-process
+        if test_response_data["type"] == "array":
+            set_component_definition(
+                OpenAPIV3SchemaParser(
+                    data={
+                        "components": {
+                            "schemas": {
+                                "FooResponse": {
+                                    "type": "object",
+                                    "required": ["id"],
+                                    "properties": {
+                                        "id": {"type": "integer", "format": "int64"},
+                                        "name": {"type": "string"},
+                                        "value1": {"type": "string"},
+                                        "value2": {"type": "string"},
+                                    },
+                                    "title": "FooResponse",
+                                },
+                            },
+                        },
+                    }
+                )
+            )
+
+        # Run target
+        resp = ut_enum.generate_response_from_data(
+            resp_prop_data=test_response_data,
+            get_schema_parser_factory=ensure_get_schema_parser_factory,
+            has_ref_callback=_YamlSchema.has_ref,
+            get_ref_callback=_YamlSchema.get_schema_ref,
+        )
+
+        # Verify
+        assert resp
+        assert resp == expected_value
 
 
 class TestConfigLoadingOrder(EnumTestSuite):
