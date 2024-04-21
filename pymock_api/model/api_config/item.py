9c3fdb39c108e82a725d3b314024bdc2b41c7e42
Chisanan232 <chi10211201@cycu.org.tw>
2024-04-21 21:46:33 +0800

    ➕ Support nested *dict* type data.

--- pymock_api/model/api_config/item.py
+++ pymock_api/model/api_config/item.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-from typing import Any, Dict, Optional
+from typing import Any, Dict, List, Optional
 
 from ._base import _Checkable, _Config
 
@@ -9,11 +9,36 @@ class IteratorItem(_Config, _Checkable):
     name: str = field(default_factory=str)
     required: Optional[bool] = None
     value_type: Optional[str] = None  # A type value as string
+    items: Optional[List["IteratorItem"]] = None
 
     _absolute_key: str = field(init=False, repr=False)
 
     def _compare(self, other: "IteratorItem") -> bool:
-        return self.name == other.name and self.required == other.required and self.value_type == other.value_type
+        return (
+            self.name == other.name
+            and self.required == other.required
+            and self.value_type == other.value_type
+            and self.items == other.items
+        )
+
+    def __post_init__(self) -> None:
+        if self.items is not None:
+            self._convert_items()
+
+    def _convert_items(self):
+        def _deserialize_item(i: dict) -> IteratorItem:
+            item = IteratorItem(
+                name=i.get("name", ""),
+                value_type=i.get("type", None),
+                required=i.get("required", True),
+                items=i.get("items", None),
+            )
+            item.absolute_model_key = self.key
+            return item
+
+        if False in list(map(lambda i: isinstance(i, (dict, IteratorItem)), self.items)):
+            raise TypeError("The data type of key *items* must be dict or IteratorItem.")
+        self.items = [_deserialize_item(i) if isinstance(i, dict) else i for i in self.items]
 
     @property
     def key(self) -> str:
@@ -31,6 +56,8 @@ class IteratorItem(_Config, _Checkable):
         }
         if name:
             serialized_data["name"] = name
+        if self.items:
+            serialized_data["items"] = [item.serialize() for item in self.items]
         return serialized_data
 
     @_Config._ensure_process_with_not_empty_value
@@ -38,6 +65,8 @@ class IteratorItem(_Config, _Checkable):
         self.name = data.get("name", None)
         self.required = data.get("required", None)
         self.value_type = data.get("type", None)
+        items = [IteratorItem().deserialize(item) for item in (data.get("items", []) or [])]
+        self.items = items if items else None
         return self
 
     def is_work(self) -> bool:
@@ -55,4 +84,13 @@ class IteratorItem(_Config, _Checkable):
             accept_empty=False,
         ):
             return False
+        if self.items:
+
+            def _i_is_work(i: IteratorItem) -> bool:
+                i.stop_if_fail = self.stop_if_fail
+                return i.is_work()
+
+            is_work_props = list(filter(lambda i: _i_is_work(i), self.items))
+            if len(is_work_props) != len(self.items):
+                return False
         return True
