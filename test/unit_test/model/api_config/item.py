fc287a0e66abd257bbd12ef6f386d96bb2e95daf
Chisanan232 <chi10211201@cycu.org.tw>
2024-04-22 10:26:54 +0800

    ✏️  Modify the test data for testing about data about data model *IteratorItem* which has nested values.

--- test/unit_test/model/api_config/item.py
+++ test/unit_test/model/api_config/item.py
@@ -4,7 +4,7 @@ import pytest
 
 from pymock_api.model.api_config import IteratorItem
 
-from ...._values import _Test_Iterable_Parameter_Item_Name
+from ...._values import _Test_Response_Property_Details_Dict
 from ._base import CheckableTestSuite, _assertion_msg, set_checking_test_data
 
 
@@ -15,9 +15,10 @@ class TestIteratorItem(CheckableTestSuite):
     @pytest.fixture(scope="function")
     def sut(self) -> IteratorItem:
         return IteratorItem(
-            name=_Test_Iterable_Parameter_Item_Name["name"],
-            required=_Test_Iterable_Parameter_Item_Name["required"],
-            value_type=_Test_Iterable_Parameter_Item_Name["type"],
+            name=_Test_Response_Property_Details_Dict["name"],
+            required=_Test_Response_Property_Details_Dict["required"],
+            value_type=_Test_Response_Property_Details_Dict["type"],
+            items=_Test_Response_Property_Details_Dict["items"],
         )
 
     @pytest.fixture(scope="function")
@@ -25,15 +26,37 @@ class TestIteratorItem(CheckableTestSuite):
         return IteratorItem()
 
     def test_value_attributes(self, sut: IteratorItem):
-        assert sut.name == _Test_Iterable_Parameter_Item_Name["name"], _assertion_msg
-        assert sut.required is _Test_Iterable_Parameter_Item_Name["required"], _assertion_msg
-        assert sut.value_type == _Test_Iterable_Parameter_Item_Name["type"], _assertion_msg
+        assert sut.name == _Test_Response_Property_Details_Dict["name"], _assertion_msg
+        assert sut.required is _Test_Response_Property_Details_Dict["required"], _assertion_msg
+        assert sut.value_type == _Test_Response_Property_Details_Dict["type"], _assertion_msg
+        if sut.items:
+            for item in sut.items:
+                assert list(
+                    filter(lambda i: i["name"] == item.name, _Test_Response_Property_Details_Dict["items"])
+                ), _assertion_msg
+                assert list(
+                    filter(lambda i: i["required"] == item.required, _Test_Response_Property_Details_Dict["items"])
+                ), _assertion_msg
+                assert list(
+                    filter(lambda i: i["type"] == item.value_type, _Test_Response_Property_Details_Dict["items"])
+                ), _assertion_msg
 
     def _expected_serialize_value(self) -> Any:
-        return _Test_Iterable_Parameter_Item_Name
+        return _Test_Response_Property_Details_Dict
 
     def _expected_deserialize_value(self, obj: IteratorItem) -> None:
         assert isinstance(obj, IteratorItem)
-        assert obj.name == _Test_Iterable_Parameter_Item_Name["name"]
-        assert obj.required is _Test_Iterable_Parameter_Item_Name["required"]
-        assert obj.value_type == _Test_Iterable_Parameter_Item_Name["type"]
+        assert obj.name == _Test_Response_Property_Details_Dict["name"]
+        assert obj.required is _Test_Response_Property_Details_Dict["required"]
+        assert obj.value_type == _Test_Response_Property_Details_Dict["type"]
+        if obj.items:
+            for item in obj.items:
+                assert list(
+                    filter(lambda i: i["name"] == item.name, _Test_Response_Property_Details_Dict["items"])
+                ), _assertion_msg
+                assert list(
+                    filter(lambda i: i["required"] == item.required, _Test_Response_Property_Details_Dict["items"])
+                ), _assertion_msg
+                assert list(
+                    filter(lambda i: i["type"] == item.value_type, _Test_Response_Property_Details_Dict["items"])
+                ), _assertion_msg
