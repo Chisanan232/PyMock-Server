6e4e5bac6efced68d19221528ffc17544def54f3
Chisanan232 <chi10211201@cycu.org.tw>
2024-04-28 22:11:19 +0800

    🧬 Extract the logic about checking the data type of parameter *path* as a common private function.

--- test/_base_test_case.py
+++ test/_base_test_case.py
@@ -20,8 +20,7 @@ class BaseTestCaseFactory(metaclass=ABCMeta):
     def _iterate_files_by_path(
         cls, path: Union[AnyStr, Tuple], generate_test_case_callback: Callable[[str], None]
     ) -> None:
-        if not isinstance(path, (str, tuple)):
-            raise TypeError("The parameter *path* only accept 'str' or 'Tuple[str]' types.")
+        cls._ensure_path_data_type(path)
         regex_files_path = path if isinstance(path, str) else str(os.path.join(*path))
         for json_config_path in glob.glob(regex_files_path):
             generate_test_case_callback(json_config_path)
@@ -48,8 +47,7 @@ class BaseTestCaseFactory(metaclass=ABCMeta):
         generate_dir_paths: Callable[[str], tuple],
         generate_test_case_callback: Callable[[tuple], None],
     ) -> None:
-        if not isinstance(path, (str, tuple)):
-            raise TypeError("The parameter *path* only accept 'str' or 'Tuple[str]' types.")
+        cls._ensure_path_data_type(path)
         regex_files_path = path if isinstance(path, str) else str(os.path.join(*path))
         for folder_path in os.listdir(regex_files_path):
             dir_paths: tuple = generate_dir_paths(folder_path)
@@ -57,3 +55,8 @@ class BaseTestCaseFactory(metaclass=ABCMeta):
                 paths=dir_paths,
                 generate_test_case_callback=generate_test_case_callback,
             )
+
+    @classmethod
+    def _ensure_path_data_type(cls, path: Union[AnyStr, Tuple]) -> None:
+        if not isinstance(path, (str, tuple)):
+            raise TypeError("The parameter *path* only accept 'str' or 'Tuple[str]' types.")
