fe1ccdd07014140ccf27054914a79980f7499584
Chisanan232 <chi10211201@cycu.org.tw>
2024-06-08 08:46:23 +0800

    👨‍💻 Add integration test for the new feature about saving configuration with dividing feature in sub-command line *add*. (not yet finish) [skip actions]

--- test/integration_test/command/add/component.py
+++ test/integration_test/command/add/component.py
@@ -1,11 +1,17 @@
-from unittest.mock import patch
+import pathlib
+from unittest.mock import PropertyMock, patch
 
 import pytest
 
 from pymock_api.command._common.component import SavingConfigComponent
 from pymock_api.command.add.component import SubCmdAddComponent
 from pymock_api.command.options import SubCommand
-from pymock_api.model import SubcmdAddArguments, deserialize_openapi_doc_config
+from pymock_api.model import (
+    SubcmdAddArguments,
+    TemplateConfig,
+    deserialize_openapi_doc_config,
+    load_config,
+)
 from pymock_api.model.enums import ResponseStrategy
 
 from ...._values import (
@@ -14,6 +20,7 @@ from ...._values import (
     _Test_HTTP_Method,
     _Test_URL,
 )
+from ._test_case import AddMockAPIAsDividingConfigTestCaseFactory, SubCmdAddTestArgs
 
 _OpenAPI_Doc_Config: dict = {
     "openapi": "3.0.2",
@@ -86,8 +93,10 @@ _OpenAPI_Doc_Config: dict = {
     },
 }
 
-
 _Dummy_Add_Arg_Strategy, _Dummy_Add_Arg_Values = _generate_response_for_add(ResponseStrategy.STRING)
+
+AddMockAPIAsDividingConfigTestCaseFactory.load()
+ADD_API_AS_DIVIDING_CONFIG_TEST_CASE = AddMockAPIAsDividingConfigTestCaseFactory.get_test_case()
 
 
 class TestSubCmdAddComponent:
@@ -278,3 +287,105 @@ class TestSubCmdAddComponent:
                     else:
                         mock_dry_run_final_process.assert_not_called()
                         mock_final_process.assert_called_once_with(cmd_args, api_config_serialize_data)
+
+    @pytest.mark.parametrize(
+        ("under_test_api_config", "cmd_arg", "expected_yaml_config_path"), ADD_API_AS_DIVIDING_CONFIG_TEST_CASE
+    )
+    def test_add_new_api_as_dividing_config(
+        self,
+        sub_cmd: SubCmdAddComponent,
+        under_test_api_config: str,
+        cmd_arg: SubCmdAddTestArgs,
+        expected_yaml_config_path: str,
+    ):
+        # Given command line argument
+        print(f"[DEBUG in test] cmd_arg: {cmd_arg}")
+        # test_scenario_dir = pathlib.Path(under_test_api_config).parent
+        # under_test_dir = "v3_openapi" if "v3" in under_test_api_config else "v2_openapi"
+        # ut_dir = pathlib.Path(test_scenario_dir, "under_test", under_test_dir)
+        # if not ut_dir.exists():
+        #     ut_dir.mkdir(parents=True)
+        print(f"[DEBUG in test] under_test_api_config: {under_test_api_config}")
+        ut_config_path = str(pathlib.Path(under_test_api_config, "api.yaml"))
+        cmd_args = SubcmdAddArguments(
+            subparser_name=SubCommand.Add,
+            config_path=ut_config_path,
+            # new mock API
+            api_path=_Test_URL,
+            http_method=_Test_HTTP_Method,
+            parameters=_Dummy_Add_Arg_Parameter,
+            response_strategy=_Dummy_Add_Arg_Strategy,
+            response_value=_Dummy_Add_Arg_Values,
+            # saving details
+            include_template_config=cmd_arg.include_template_config,
+            base_file_path=str(under_test_api_config),
+            dry_run=False,
+            divide_api=cmd_arg.divide_api,
+            divide_http=cmd_arg.divide_http,
+            divide_http_request=cmd_arg.divide_http_request,
+            divide_http_response=cmd_arg.divide_http_response,
+        )
+
+        # new_api_config_has_new_api = YAML().read(path=under_test_api_config)
+        # with open(under_test_api_config, "r") as file:
+        #     new_api_config_has_new_api = json.loads(file.read())
+
+        # Note: Set the base file path to let the test could run and save the result configuration under the target
+        # directory
+        with patch(
+            "pymock_api.model.api_config.MockAPIs.template", new_callable=PropertyMock
+        ) as mock_mock_apis_template:
+            template_config = TemplateConfig()
+            template_config.values.base_file_path = str(under_test_api_config)
+            mock_mock_apis_template.return_value = template_config
+
+            # Set the Swagger API reference data for testing
+            # set_component_definition(OpenAPIV2SchemaParser(data=new_api_config_has_new_api))
+            # Mock the HTTP request result as the Swagger API documentation data
+            # with patch(
+            #     "pymock_api.command.pull.component.URLLibHTTPClient.request", return_value=new_api_config_has_new_api
+            # ) as mock_swagger_request:
+            # Run target function
+            sub_cmd.process(args=cmd_args)
+
+            # Expected values
+            expected_config_data_modal = load_config(expected_yaml_config_path, is_pull=True)
+
+            # Verify
+            # mock_swagger_request.assert_called_once()
+            ut_config_data_modal = load_config(under_test_api_config, is_pull=True)
+            assert ut_config_data_modal is not None
+            assert expected_config_data_modal is not None
+
+            # Basic configuration
+            assert ut_config_data_modal.name == expected_config_data_modal.name
+            assert ut_config_data_modal.description == expected_config_data_modal.description
+
+            # mock APIs configuration
+            assert ut_config_data_modal.apis is not None
+            assert expected_config_data_modal.apis is not None
+            assert ut_config_data_modal.apis.base == expected_config_data_modal.apis.base
+            assert ut_config_data_modal.apis.apis.keys() == expected_config_data_modal.apis.apis.keys()
+            for api_key in ut_config_data_modal.apis.apis.keys():
+                ut_api = ut_config_data_modal.apis.apis[api_key]
+                expect_api = expected_config_data_modal.apis.apis[api_key]
+
+                # Basic checking
+                assert ut_api is not None
+                assert expect_api is not None
+                assert ut_api.http is not None
+                assert expect_api.http is not None
+                assert ut_api.http.request is not None
+                assert ut_api.http.response is not None
+                assert expect_api.http.request is not None
+                assert expect_api.http.response is not None
+
+                # Details checking
+                assert ut_api.url == expect_api.url
+                assert ut_api.tag == expect_api.tag
+                assert ut_api.http.request.method == expect_api.http.request.method
+                assert ut_api.http.request.parameters == expect_api.http.request.parameters
+                assert ut_api.http.response.strategy == expect_api.http.response.strategy
+                assert ut_api.http.response.value == expect_api.http.response.value
+                assert ut_api.http.response.path == expect_api.http.response.path
+                assert ut_api.http.response.properties == expect_api.http.response.properties
