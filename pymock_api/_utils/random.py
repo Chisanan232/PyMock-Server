e42d16d02ee7d3d16a147572b4134c102fea1809
Chisanan232 <chi10211201@cycu.org.tw>
2024-07-02 14:47:32 +0800

    ✏️  Modify the return value data type as correct.

--- pymock_api/_utils/random.py
+++ pymock_api/_utils/random.py
@@ -17,11 +17,11 @@ class BaseRandomGenerator(metaclass=ABCMeta):
 
 class RandomString(BaseRandomGenerator):
     @staticmethod
-    def generate(size: int = 10) -> Any:
+    def generate(size: int = 10) -> str:
         return "".join([random.choice(string.ascii_letters) for _ in range(size)])
 
 
 class RandomInteger(BaseRandomGenerator):
     @staticmethod
-    def generate(min_value: int = -127, max_value: int = 128) -> Any:
+    def generate(min_value: int = -127, max_value: int = 128) -> int:
         return random.randint(min_value, max_value)
