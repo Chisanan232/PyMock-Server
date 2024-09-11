ba53a5018bebf6379fb57a948ffd999460d6a04f
Chisanan232 <chi10211201@cycu.org.tw>
2024-09-11 21:58:09 +0800

    🧬 Remove deprecated and unused code.

--- pymock_api/model/openapi/_parser.py
+++ pymock_api/model/openapi/_parser.py
@@ -26,15 +26,5 @@ class OpenAPIDocumentConfigParser(BaseParser):
     def parser(self) -> BaseOpenAPISchemaParser:
         return cast(BaseOpenAPISchemaParser, super().parser)
 
-    def process_paths(self, data_modal: Type[BaseOpenAPIDataModel]) -> List[BaseOpenAPIDataModel]:
-        apis = self.parser.get_paths()
-        paths = []
-        for api_path, api_props in apis.items():
-            for one_api_http_method, one_api_details in api_props.items():
-                paths.append(
-                    data_modal.generate(api_path=api_path, http_method=one_api_http_method, detail=one_api_details)
-                )
-        return paths
-
     def process_tags(self, data_modal: Type[BaseOpenAPIDataModel]) -> List[BaseOpenAPIDataModel]:
         return list(map(lambda t: data_modal.generate(t), self.parser.get_tags()))
