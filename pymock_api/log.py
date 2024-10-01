557e8ef284bdfabb720c541dafe00f37648c17ab
Chisanan232 <chi10211201@cycu.org.tw>
2024-10-01 22:10:19 +0800

    ➕ Add log module which focus on processing log logic to centralize to manage it.

---
+++ pymock_api/log.py
@@ -0,0 +1,15 @@
+import logging
+
+
+def init_logger_config(
+    format: str = "%(asctime)s %(levelname)s %(message)s",
+    datefmt: str = "%Y-%m-%d %H:%M:%S",
+    level: int = logging.WARNING,
+    encoding: str = "utf-8",
+) -> None:
+    logging.basicConfig(
+        format=format,
+        datefmt=datefmt,
+        level=level,
+        encoding=encoding,
+    )
