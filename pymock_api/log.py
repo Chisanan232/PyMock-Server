dbc6192d536477436942b1d61f35918f3af627a6
Chisanan232 <chi10211201@cycu.org.tw>
2024-10-01 22:45:14 +0800

    ✏️ Modify the date time value format to be more clear and readable.

--- pymock_api/log.py
+++ pymock_api/log.py
@@ -3,7 +3,7 @@ import logging
 
 def init_logger_config(
     format: str = "%(asctime)s [%(levelname)s] (%(name)s): %(message)s",
-    datefmt: str = "%Y-%m-%d %H:%M:%S",
+    datefmt: str = "%Y-%m-%d %H:%M:%S UTC%z",
     level: int = logging.WARNING,
     encoding: str = "utf-8",
 ) -> None:
