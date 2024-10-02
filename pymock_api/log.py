1d181fe93473c03a169c71060ddebadfaeb15215
Chisanan232 <chi10211201@cycu.org.tw>
2024-10-02 20:16:29 +0800

    ✏️ Modify the setting logic at properties log format and log datetime format for different log level.

--- pymock_api/log.py
+++ pymock_api/log.py
@@ -1,12 +1,19 @@
 import logging
 
+DEBUG_LEVEL_LOG_FORMAT: str = "%(asctime)s [%(levelname)s] (%(name)s): %(message)s"
+DEBUG_LEVEL_LOG_DATETIME_FORMAT: str = "%Y-%m-%d %H:%M:%S UTC%z"
+
 
 def init_logger_config(
-    format: str = "%(asctime)s [%(levelname)s] (%(name)s): %(message)s",
-    datefmt: str = "%Y-%m-%d %H:%M:%S UTC%z",
+    format: str = "",
+    datefmt: str = "",
     level: int = logging.WARNING,
     encoding: str = "utf-8",
 ) -> None:
+    if level is logging.DEBUG:
+        format = DEBUG_LEVEL_LOG_FORMAT
+        datefmt = DEBUG_LEVEL_LOG_DATETIME_FORMAT
+
     logging.basicConfig(
         format=format,
         datefmt=datefmt,
