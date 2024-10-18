3b7233bba34753c28a25fece4ac9f8fc29361344
Chisanan232 <chi10211201@cycu.org.tw>
2024-10-18 12:02:14 +0800

    ➕ Add new data model about saving the current using argument info in system.

--- pymock_server/command/options.py
+++ pymock_server/command/options.py
@@ -46,6 +46,41 @@ def make_options() -> List["CommandOption"]:
     return mock_api_cmd_options
 
 
+@dataclass
+class SysArg:
+    subcmd: str
+    pre_subcmd: Optional["SysArg"] = None
+
+    @staticmethod
+    def parse(args: List[str]) -> "SysArg":
+        if not args:
+            return SysArg(subcmd="base")
+
+        no_pyfile_subcmds = list(filter(lambda a: not re.search(r".{1,1024}.py", a), args))
+        subcmds = []
+        for subcmd_or_options in no_pyfile_subcmds:
+            search_subcmd = re.search(r"-.{1,256}", subcmd_or_options)
+            if search_subcmd and len(search_subcmd.group(0)) == len(subcmd_or_options):
+                break
+            subcmds.append(subcmd_or_options)
+
+        if len(subcmds) == 0:
+            return SysArg(subcmd="base")
+        elif len(subcmds) == 1:
+            return SysArg(
+                pre_subcmd=SysArg(
+                    pre_subcmd=None,
+                    subcmd="base",
+                ),
+                subcmd=subcmds[0],
+            )
+        else:
+            return SysArg(
+                pre_subcmd=SysArg.parse(subcmds[:-1]),
+                subcmd=subcmds[-1],
+            )
+
+
 class MockAPICommandParser:
     """*The parser of PyMock-API command line*
 
