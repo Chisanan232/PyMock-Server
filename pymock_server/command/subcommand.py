1ac5eeea43f4e90d9de4d7995adfd50684d5a37f
Chisanan232 <chi10211201@cycu.org.tw>
2024-11-05 19:48:15 +0800

    ➕ Add new enum object about subcommand line interface.

--- pymock_server/command/subcommand.py
+++ pymock_server/command/subcommand.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from enum import Enum
+from typing import Union
 
 
 @dataclass
@@ -12,6 +13,26 @@ class SubCommand:
     Get: str = "get"
     Sample: str = "sample"
     Pull: str = "pull"
+
+
+class SubCommandLine(Enum):
+    Base: str = "subcommand"
+    RestServer: str = "rest-server"
+    Run: str = "run"
+    Add: str = "add"
+    Check: str = "check"
+    Get: str = "get"
+    Sample: str = "sample"
+    Pull: str = "pull"
+
+    @staticmethod
+    def to_enum(v: Union[str, "SubCommandLine"]) -> "SubCommandLine":
+        if isinstance(v, SubCommandLine):
+            return v
+        for subcmd in SubCommandLine:
+            if subcmd.value.lower() == v.lower():
+                return subcmd
+        raise ValueError(f"Cannot map anyone subcommand line with value '{v}'.")
 
 
 class SubCommandSection(Enum):
