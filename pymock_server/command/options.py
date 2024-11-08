713f043598cedce3076dc89a5a0a3599ce902bc1
Chisanan232 <chi10211201@cycu.org.tw>
2024-11-08 16:33:12 +0800

    ♻️ Migrate the command line option models of subcommand line *rest-server sample* into new single module in sub-package *rest-server.sample* for maintainable and clear.

--- pymock_server/command/options.py
+++ pymock_server/command/options.py
@@ -13,9 +13,11 @@ briefly, It has below major features:
 """
 
 import argparse
-from typing import Any, List, Optional
+from typing import Any, List
 
 from pymock_server.__pkg_info__ import __version__
+
+# NOTE: Just for importing the command line options, do nothing in this module
 from pymock_server.command.rest_server.add.options import (
     import_option as import_add_options,
 )
@@ -28,12 +30,13 @@ from pymock_server.command.rest_server.get.options import (
 from pymock_server.command.rest_server.pull.options import (
     import_option as import_pull_options,
 )
-
-# NOTE: Just for importing the command line options, do nothing in this module
 from pymock_server.command.rest_server.run.options import (
     import_option as import_run_options,
 )
-from pymock_server.model.subcmd_common import SubCommandAttr, SubParserAttr
+from pymock_server.command.rest_server.sample.options import (
+    import_option as import_sample_options,
+)
+from pymock_server.model.subcmd_common import SubCommandAttr
 
 from ._base_options import (
     CommandLineOptions,
@@ -41,7 +44,6 @@ from ._base_options import (
     MetaCommandOption,
     SubCommandInterface,
 )
-from .rest_server.option import BaseSubCommandRestServer
 from .subcommand import SubCommandLine, SubCommandSection
 
 # FIXME: Please use more clear and beautiful implementation to apply the command line options
@@ -50,6 +52,7 @@ import_add_options()
 import_check_options()
 import_get_options()
 import_pull_options()
+import_sample_options()
 
 
 def get_all_subcommands() -> List[str]:
@@ -81,15 +84,7 @@ class BaseSubCommand(CommandOption):
     )
 
 
-class SubCommandSampleOption(BaseSubCommandRestServer):
-    sub_parser: SubParserAttr = SubParserAttr(
-        name=SubCommandLine.Sample,
-        help="Quickly display or generate a sample configuration helps to use this tool.",
-    )
-
-
 BaseCmdOption: type = MetaCommandOption("BaseCmdOption", (CommandOption,), {})
-BaseSubCmdSampleOption: type = MetaCommandOption("BaseSubCmdSampleOption", (SubCommandSampleOption,), {})
 
 
 class Version(BaseCmdOption):
@@ -110,41 +105,3 @@ class Version(BaseCmdOption):
             "version": self._version_output,
         }
         parser.add_argument(*self.cli_option_name, **cmd_option_args)
-
-
-class PrintSample(BaseSubCmdSampleOption):
-    cli_option: str = "-p, --print-sample"
-    name: str = "print_sample"
-    help_description: str = "Print the sample configuration content."
-    action: str = "store_true"
-    option_value_type: Optional[type] = None
-    default_value: bool = False
-
-
-class GenerateSample(BaseSubCmdSampleOption):
-    cli_option: str = "-g, --generate-sample"
-    name: str = "generate_sample"
-    help_description: str = "Create a sample configuration file."
-    action: str = "store_true"
-    option_value_type: Optional[type] = None
-    default_value: bool = False
-
-
-class Output(BaseSubCmdSampleOption):
-    cli_option: str = "-o, --output"
-    name: str = "file_path"
-    help_description: str = (
-        "Save the sample configuration to this path. In generally, this option would be used with"
-        " option *-g* (aka *--generate-sample*)."
-    )
-    option_value_type: type = str
-    default_value: str = "sample-api.yaml"
-
-
-class DemoSampleType(BaseSubCmdSampleOption):
-    cli_option: str = "-t, --sample-config-type"
-    name: str = "sample_config_type"
-    help_description: str = "Which configuration type (the type means the response way) you want to demonstrate."
-    option_value_type: type = str
-    default_value: str = "all"
-    _options: List[str] = ["all", "response_as_str", "response_as_json", "response_with_file"]
