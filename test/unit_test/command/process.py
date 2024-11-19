6c308ae0b43bc393fb5ba2b519b5c478812cfb29
Chisanan232 <chi10211201@cycu.org.tw>
2024-11-19 19:15:06 +0800

    ♻️ Migrate the test suite of subcommand line *rest-server run* into new single test module in sub-package *rest_server.run*.

--- test/unit_test/command/process.py
+++ test/unit_test/command/process.py
@@ -37,22 +37,18 @@ from test._values import (
     _Sample_Data_Type,
     _Sample_File_Path,
     _Show_Detail_As_Format,
-    _Test_App_Type,
-    _Test_Auto_Type,
     _Test_Config,
     _Test_Divide_Api,
     _Test_Divide_Http,
     _Test_Divide_Http_Request,
     _Test_Divide_Http_Response,
     _Test_Dry_Run,
-    _Test_FastAPI_App_Type,
     _Test_HTTP_Method,
     _Test_HTTP_Resp,
     _Test_Request_With_Https,
     _Test_Response_Strategy,
     _Test_SubCommand_Check,
     _Test_SubCommand_Get,
-    _Test_SubCommand_Run,
     _Test_URL,
     _Workers_Amount,
 )
@@ -67,7 +63,6 @@ from pymock_server.command.rest_server.add.process import SubCmdAdd
 from pymock_server.command.rest_server.check.process import SubCmdCheck
 from pymock_server.command.rest_server.get.process import SubCmdGet
 from pymock_server.command.rest_server.pull.process import SubCmdPull
-from pymock_server.command.rest_server.run.process import SubCmdRun
 from pymock_server.command.rest_server.sample.process import SubCmdSample
 from pymock_server.command.subcommand import SubCommandLine
 from pymock_server.model import (
@@ -83,7 +78,7 @@ from pymock_server.model import (
 from pymock_server.model._sample import SampleType
 from pymock_server.model.api_config.apis import ResponseStrategy
 from pymock_server.model.subcmd_common import SysArg
-from pymock_server.server import ASGIServer, Command, CommandOptions, WSGIServer
+from pymock_server.server import Command, CommandOptions, WSGIServer
 
 from ._test_case import SubCmdGetTestCaseFactory, SubCmdPullTestCaseFactory
 
@@ -285,100 +280,6 @@ class TestNoSubCmd(BaseCommandProcessorTestSpec):
 
     def _expected_argument_type(self) -> Type[Namespace]:
         return Namespace
-
-
-class TestSubCmdRun(BaseCommandProcessorTestSpec):
-    @pytest.fixture(scope="function")
-    def cmd_ps(self) -> SubCmdRun:
-        return SubCmdRun()
-
-    @pytest.mark.parametrize(
-        ("app_type", "should_raise_exc"),
-        [
-            (_Test_App_Type, False),
-            (_Test_FastAPI_App_Type, False),
-            (_Test_Auto_Type, False),
-            ("invalid app-type which is not a Python web library or framework", True),
-        ],
-    )
-    def test_with_command_processor(self, app_type: str, should_raise_exc: bool, object_under_test: Callable):
-        kwargs = {
-            "app_type": app_type,
-            "should_raise_exc": should_raise_exc,
-            "cmd_ps": object_under_test,
-        }
-        self._test_process(**kwargs)
-
-    @pytest.mark.parametrize(
-        ("app_type", "should_raise_exc"),
-        [
-            (_Test_App_Type, False),
-            (_Test_FastAPI_App_Type, False),
-            (_Test_Auto_Type, False),
-            ("invalid app-type which is not a Python web library or framework", True),
-        ],
-    )
-    def test_with_run_entry_point(self, app_type: str, should_raise_exc: bool, entry_point_under_test: Callable):
-        kwargs = {
-            "app_type": app_type,
-            "should_raise_exc": should_raise_exc,
-            "cmd_ps": entry_point_under_test,
-        }
-        self._test_process(**kwargs)
-
-    def _test_process(self, app_type: str, should_raise_exc: bool, cmd_ps: Callable):
-        mock_parser_arg = _given_parser_args(subcommand=_Test_SubCommand_Run, app_type=app_type)
-        command = _given_command(app_type="Python web library")
-        command.run = MagicMock()
-        cmd_parser = Mock()
-
-        with patch.object(sys, "argv", self._given_command_line()):
-            with patch.object(ASGIServer, "generate", return_value=command) as mock_asgi_generate:
-                with patch.object(WSGIServer, "generate", return_value=command) as mock_wsgi_generate:
-                    if should_raise_exc:
-                        with pytest.raises(ValueError) as exc_info:
-                            cmd_ps(cmd_parser, mock_parser_arg)
-                        assert "Invalid value" in str(exc_info.value)
-                        mock_asgi_generate.assert_not_called()
-                        mock_wsgi_generate.assert_not_called()
-                        command.run.assert_not_called()
-                    else:
-                        cmd_ps(cmd_parser, mock_parser_arg)
-                        if app_type == "auto":
-                            mock_asgi_generate.assert_called_once_with(mock_parser_arg)
-                            mock_wsgi_generate.assert_not_called()
-                        elif app_type == "flask":
-                            mock_asgi_generate.assert_not_called()
-                            mock_wsgi_generate.assert_called_once_with(mock_parser_arg)
-                        elif app_type == "fastapi":
-                            mock_asgi_generate.assert_called_once_with(mock_parser_arg)
-                            mock_wsgi_generate.assert_not_called()
-                        else:
-                            assert False, "Please use valid *app-type* option value."
-                        command.run.assert_called_once()
-
-    def _given_command_line(self) -> List[str]:
-        return ["rest-server", "run"]
-
-    def _given_cmd_args_namespace(self) -> Namespace:
-        args_namespace = Namespace()
-        args_namespace.subcommand = SubCommand.RestServer
-        setattr(args_namespace, SubCommand.RestServer, SubCommand.Run)
-        args_namespace.config = _Test_Config
-        args_namespace.app_type = _Test_App_Type
-        args_namespace.bind = _Bind_Host_And_Port.value
-        args_namespace.workers = _Workers_Amount.value
-        args_namespace.log_level = _Log_Level.value
-        return args_namespace
-
-    def _given_subcmd(self) -> Optional[SysArg]:
-        return SysArg(
-            pre_subcmd=SysArg(pre_subcmd=SysArg(subcmd=SubCommandLine.Base), subcmd=SubCommandLine.RestServer),
-            subcmd=SubCommandLine.Run,
-        )
-
-    def _expected_argument_type(self) -> Type[SubcmdRunArguments]:
-        return SubcmdRunArguments
 
 
 class TestSubCmdAdd(BaseCommandProcessorTestSpec):
