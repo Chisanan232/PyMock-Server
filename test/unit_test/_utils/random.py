8dbdc7bdf65eb01d3d69453fc9718c753879a160
Chisanan232 <chi10211201@cycu.org.tw>
2024-12-13 22:07:01 +0800

    ➕ Add new random generators about randomly generates date or datetime format value.

--- test/unit_test/_utils/random.py
+++ test/unit_test/_utils/random.py
@@ -1,4 +1,6 @@
+import datetime
 import re
+from abc import abstractmethod
 from typing import Type
 
 import pytest
@@ -7,6 +9,8 @@ from pymock_server._utils.random import (
     BaseRandomGenerator,
     RandomBigDecimal,
     RandomBoolean,
+    RandomDate,
+    RandomDateTime,
     RandomFromSequence,
     RandomInteger,
     RandomString,
@@ -20,3 +24,41 @@ def test_invalid_usage(random_obj: Type[BaseRandomGenerator]):
     with pytest.raises(RuntimeError) as exc_info:
         random_obj()
     assert re.search(r"don't instantiate", str(exc_info.value)) is not None
+
+
+class BaseRandomGeneratorTestSuite:
+
+    @pytest.fixture(scope="function")
+    @abstractmethod
+    def generator(self) -> Type[BaseRandomGenerator]:
+        pass
+
+    @abstractmethod
+    def test_generate(self, generator: Type[BaseRandomGenerator]):
+        pass
+
+
+class TestRandomDate(BaseRandomGeneratorTestSuite):
+    @pytest.fixture(scope="function")
+    def generator(self) -> Type[RandomDate]:
+        return RandomDate
+
+    def test_generate(self, generator: Type[RandomDate]):
+        random_date = generator.generate()
+        assert re.search(r"\d{4}-\d{1,2}-\d{1,2}", random_date)
+        now = datetime.datetime.now()
+        random_date_obj = datetime.datetime.strptime(random_date, "%Y-%m-%d")
+        assert now - datetime.timedelta(days=30) <= random_date_obj <= now + datetime.timedelta(days=0)
+
+
+class TestRandomDateTime(BaseRandomGeneratorTestSuite):
+    @pytest.fixture(scope="function")
+    def generator(self) -> Type[RandomDateTime]:
+        return RandomDateTime
+
+    def test_generate(self, generator: Type[RandomDateTime]):
+        random_date = generator.generate()
+        assert re.search(r"\d{4}-\d{1,2}-\d{1,2}T\d{1,2}:\d{1,2}:\d{1,2}Z", random_date)
+        now = datetime.datetime.now()
+        random_date_obj = datetime.datetime.strptime(random_date, "%Y-%m-%dT%H:%M:%SZ")
+        assert now - datetime.timedelta(days=30) <= random_date_obj <= now + datetime.timedelta(days=0)
