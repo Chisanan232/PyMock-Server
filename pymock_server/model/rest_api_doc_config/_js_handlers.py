84315a747455523657040a525b73cf80faee385f
Chisanan232 <chi10211201@cycu.org.tw>
2024-12-13 15:55:49 +0800

    ➕ Add new enum object about the value format types of API documentation spec.

--- pymock_server/model/rest_api_doc_config/_js_handlers.py
+++ pymock_server/model/rest_api_doc_config/_js_handlers.py
@@ -1,3 +1,7 @@
+from enum import Enum
+from typing import Union
+
+
 def convert_js_type(t: str) -> str:
     if t == "string":
         return "str"
@@ -20,3 +24,21 @@ def ensure_type_is_python_type(t: str) -> str:
     if t in ["string", "integer", "number", "boolean", "array", "object"]:
         return convert_js_type(t)
     return t
+
+
+class ApiDocValueFormat(Enum):
+    DateTime: str = "date-time"
+    Int32: str = "int32"
+    Int64: str = "int64"
+    Float: str = "float"
+    Double: str = "double"
+    Password: str = "password"
+
+    @staticmethod
+    def to_enum(v: Union[str, "ApiDocValueFormat"]) -> "ApiDocValueFormat":
+        if isinstance(v, ApiDocValueFormat):
+            return v
+        for formatter in ApiDocValueFormat:
+            if formatter.value.lower() == v.lower():
+                return formatter
+        raise ValueError(f"Cannot map anyone format with value '{v}'.")
--- test/unit_test/model/rest_api_doc_config/inner_js_handlers.py
+++ test/unit_test/model/rest_api_doc_config/inner_js_handlers.py
@@ -1,6 +1,11 @@
+from typing import Union
+
 import pytest
 
-from pymock_server.model.rest_api_doc_config._js_handlers import convert_js_type
+from pymock_server.model.rest_api_doc_config._js_handlers import (
+    ApiDocValueFormat,
+    convert_js_type,
+)
 
 
 @pytest.mark.parametrize(
@@ -23,3 +28,30 @@ def test_fail_convert_js_type():
     with pytest.raises(TypeError) as exc_info:
         convert_js_type("JS type which does not support to convert")
     assert "cannot parse JS type" in str(exc_info.value)
+
+
+class TestApiDocValueFormat:
+
+    @pytest.mark.parametrize(
+        ("under_test_value", "expected_value"),
+        [
+            (ApiDocValueFormat.DateTime, ApiDocValueFormat.DateTime),
+            (ApiDocValueFormat.Int32, ApiDocValueFormat.Int32),
+            (ApiDocValueFormat.Int64, ApiDocValueFormat.Int64),
+            (ApiDocValueFormat.Float, ApiDocValueFormat.Float),
+            (ApiDocValueFormat.Double, ApiDocValueFormat.Double),
+            (ApiDocValueFormat.Password, ApiDocValueFormat.Password),
+            ("date-time", ApiDocValueFormat.DateTime),
+            ("date-Time", ApiDocValueFormat.DateTime),
+            ("int64", ApiDocValueFormat.Int64),
+            ("INT32", ApiDocValueFormat.Int32),
+        ],
+    )
+    def test_to_enum(
+        self, under_test_value: Union[str, ApiDocValueFormat.Int32], expected_value: ApiDocValueFormat.Int32
+    ):
+        assert ApiDocValueFormat.to_enum(under_test_value) is expected_value
+
+    def test_to_enum_with_invalid_value(self):
+        with pytest.raises(ValueError):
+            ApiDocValueFormat.to_enum("invalid value")
