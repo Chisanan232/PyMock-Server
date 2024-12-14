0590a1dedbd6ae1ea658e71f5fac771aabb68af9
Chisanan232 <chi10211201@cycu.org.tw>
2024-12-14 14:29:59 +0800

    ➕ Add new enum object about each different URI schemas.

---
+++ pymock_server/_utils/uri_protocol.py
@@ -0,0 +1,25 @@
+from enum import Enum
+from typing import Union
+
+
+class URISchema(Enum):
+    HTTP: str = "http"
+    HTTPS: str = "https"
+    File: str = "file"
+    FTP: str = "ftp"
+    Mail_To: str = "mailto"
+    LDAP: str = "ldap"
+    NEWS: str = "news"
+    TEL: str = "tel"
+    TELNET: str = "telnet"
+    URN: str = "urn"
+
+    @staticmethod
+    def to_enum(v: Union[str, "URISchema"]):
+        if isinstance(v, URISchema):
+            return v
+        else:
+            for schema in URISchema:
+                if schema.value.lower() == str(v).lower():
+                    return schema
+            raise ValueError(f"Cannot find the URI schema '{v}'.")
