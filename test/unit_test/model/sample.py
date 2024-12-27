72e665e3060d781b74b331c319f2ac413d73a164
Chisanan232 <chi10211201@cycu.org.tw>
2024-12-27 13:29:47 +0800

    ➕ Add new tests for verifying the sample config is valid for PyMock-Server.

--- test/unit_test/model/sample.py
+++ test/unit_test/model/sample.py
@@ -2,6 +2,7 @@ import re
 
 import pytest
 
+from pymock_server.model import generate_empty_config
 from pymock_server.model._sample import SampleType, get_sample_by_type
 
 
@@ -27,3 +28,18 @@ def test_get_sample_by_invalid_type(invalid_st: SampleType):
     with pytest.raises(ValueError) as exc_info:
         get_sample_by_type(invalid_st)
     assert re.search(r".{0,64}doesn't support.{0,64}" + re.escape(invalid_st), str(exc_info.value), re.IGNORECASE)
+
+
+@pytest.mark.parametrize("st", SampleType)
+def test_get_sample_is_correct_usage(st: SampleType):
+    sample_data = get_sample_by_type(st)
+    config_has_data = generate_empty_config().deserialize(sample_data)
+
+    if st is SampleType.ALL:
+        assert len(config_has_data.apis.apis.keys()) == len(list(filter(lambda t: t is not SampleType.ALL, SampleType)))
+        all_sample_types = list(filter(lambda t: t is not SampleType.ALL, SampleType))
+        for sample_type in all_sample_types:
+            assert sample_type.value in config_has_data.apis.apis.keys()
+    else:
+        assert len(config_has_data.apis.apis.keys()) == 1
+        assert st.value in config_has_data.apis.apis.keys()
